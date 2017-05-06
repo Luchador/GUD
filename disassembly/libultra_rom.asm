2484285ab055d852354c25c3120eb1b2e55a51ef
Larry <kholdfuzion@gmail.com>
2017-05-06 09:21:21 -0500

    set vaddr labels for sdk functions that were grabbed by running a signature, names to be fixed

--- disassembly/libultra_rom.asm
+++ disassembly/libultra_rom.asm
@@ -21,3 +21,820 @@ osCreateThread:
 
 base $7000D580
 osStartThread:
+
+base $7000D6D0
+osCreateMesgQueue:
+
+base $7000D740
+osStopThread:
+
+base $7000D800
+osSetThreadPri:
+
+base $7000D8E0
+osGetCount:
+
+base $7000D8F0
+osCreateViManager:
+
+base $7000DC50
+osSetEventMesg:
+
+base $7000DCC0
+osViSetEvent:
+
+base $7000DD30
+osSetIntMask:
+
+base $7000DDD0
+osRecvMesg:
+
+base $7000DF10
+osSendMesg:
+
+base $7000E060
+osViSetMode:
+
+base $7000E0D0
+osViSetXScale:
+
+base $7000E200
+osViSetYScale:
+
+base $7000E260
+osViRepeatLine:
+
+base $7000E2D0
+osViBlack:
+
+base $7000E340
+osSpTaskYielded:
+
+base $7000E3C0
+osDpGetCounters:
+
+base $7000E490
+osViSwapBuffer:
+
+base $7000E4E0
+osDpSetStatus:
+
+base $7000E60C
+osSpTaskLoad:
+
+base $7000E76C
+osSpTaskStartGo:
+
+base $7000E7B0
+osDpSetNextBuffer:
+
+base $7000E860
+osSpTaskYield:
+
+base $7000E880
+__osGetTLBHi:
+
+base $7000E8B0
+osVirtualToPhysical:
+
+base $7000E930
+osAiSetFrequency:
+
+base $7000EA90
+alUnlink:
+
+base $7000EAC0
+alLink:
+
+base $7000EAE4
+alClose:
+
+base $7000EB1C
+alInit:
+
+base $7000EB50
+alHeapDBAlloc:
+
+base $7000EBB0
+osGetTime:
+
+base $7000EC40
+__ull_rshift:
+
+base $7000EC6C
+__ull_rem:
+
+base $7000ECA8
+__ull_div:
+
+base $7000ECE4
+__ll_lshift:
+
+base $7000ED10
+__ll_rem:
+
+base $7000ED4C
+__ll_div:
+
+base $7000EDA8
+__ll_mul:
+
+base $7000EDD8
+__ull_divremi:
+
+base $7000EE38
+__ll_mod:
+
+base $7000EED4
+__ll_rshift:
+
+base $7000EF00
+osAiSetNextBuffer:
+
+base $7000EFB0
+osAiGetLength:
+
+base $7000EFC8
+_timeToSamples:
+
+base $7000F020
+_freePVoice:
+
+base $7000F058
+_collectPVoices:
+
+base $7000F0B8
+__freeParam:
+
+base $7000F0D0
+__allocParam:
+
+base $7000F108
+alAudioFrame:
+
+base $7000F3A0
+alSynNew:
+
+base $7000F6A0
+osPiStartDma_0:
+
+base $7000F7B0
+osViSetSpecialFeatures:
+
+base $7000F970
+_bcopy:
+
+base $7000FC80
+guPerspectiveF:
+
+base $7000FEB0
+guPerspective:
+
+base $7000FF10
+guMtxF2L:
+
+base $70010010
+guMtxIdentF:
+
+base $70010098
+guMtxIdent:
+
+base $700100C8
+guMtxL2F:
+
+base $70010190
+__osExceptionPreamble:
+
+base $700101A0
+__osException:
+
+base $700106C4
+send_mesg:
+
+base $70010778
+handle_CpU:
+
+base $700107AC
+__osEnqueueAndYield:
+
+base $700108AC
+__osEnqueueThread:
+
+base $700108F4
+__osPopThread:
+
+base $70010904
+__osDispatchThread:
+
+base $70010A80
+__osCleanupThread:
+
+base $70010A90
+__osDequeueThread:
+
+base $70010AD0
+osYieldThread:
+
+base $70010B20
+osInvalDCache:
+
+base $70010BD0
+osWritebackDCache:
+
+base $70010C50
+osSetTimer:
+
+base $70010D30
+alHeapInit:
+
+base $70010E74
+alBnkfNew:
+
+base $70010F78
+alSeqFileNew:
+
+base $70010FC0
+__CSPPostNextSeqEvent:
+
+base $70011A54
+def_70011278:
+
+base $7001202C
+def_70011B04:
+
+base $70012080
+alCSPNew:
+
+base $700121F0
+unknown_libname_8:
+
+base $70012230
+unknown_libname_9:
+
+base $70012240
+alCSeqGetLoc:
+
+base $700122E0
+alCSeqSetLoc:
+
+base $700124A4
+alCSeqNextEvent:
+
+base $7001279C
+alCSeqNew:
+
+base $700128B0
+alCSeqNewMarker:
+
+base $70012A1C
+alCSeqGetTicks:
+
+base $70012A24
+alCSeqSecToTicks:
+
+base $70012B20
+alCSeqTicksToSec:
+
+base $70012B8C
+__alCSeqNextDelta:
+
+base $70012D00
+unknown_libname_10:
+
+base $70012D40
+unknown_libname_11:
+
+base $70012D70
+unknown_libname_12:
+
+base $70012DA0
+unknown_libname_13:
+
+base $70012DE0
+alEvtqFlushType:
+
+base $70012E8C
+alEvtqFlush:
+
+base $70012EFC
+alEvtqPostEvent:
+
+base $70013020
+alEvtqNextEvent:
+
+base $700130AC
+alEvtqNew:
+
+base $70013120
+alSynAddPlayer:
+
+base $70013170
+_allocatePVoice:
+
+base $70013258
+alSynAllocVoice:
+
+base $700133A0
+alSynSetVol:
+
+base $70013440
+alSynStartVoice:
+
+base $700134D0
+alSynSetPan:
+
+base $70013560
+alSynSetPitch:
+
+base $700135F0
+alSynSetFXMix:
+
+base $70013690
+alSynStopVoice:
+
+base $70013710
+alSynFreeVoice:
+
+base $700137C0
+alCents2Ratio:
+
+base $70013810
+osPiReadIo:
+
+base $70013850
+memcpy:
+
+base $7001387C
+strlen:
+
+base $700138A4
+strchr:
+
+base $70013F38
+def_70013944:
+
+base $70013F60
+_Printf:
+
+base $700145B0
+osCreatePiManager:
+
+base $70014740
+osPfsInit:
+
+base $700147F4
+__osPfsGetStatus:
+
+base $70014900
+osContInit:
+
+base $70014AF8
+__osContGetInitData:
+
+base $70014BC8
+__osPackRequestData:
+
+base $70014CC0
+osContStartQuery:
+
+base $70014D44
+osContGetQuery:
+
+base $70014D70
+osContStartReadData:
+
+base $70014E34
+osContGetReadData:
+
+base $70014FD0
+osEepromProbe:
+
+base $70015040
+osEepromRead:
+
+base $70015340
+osEepromWrite:
+
+base $700155FC
+__osEepStatus:
+
+base $70015820
+osEepromLongRead:
+
+base $70015960
+osEepromLongWrite:
+
+base $70015AA0
+osPfsIsPlug:
+
+base $70015C40
+__osPfsRequestData:
+
+base $70015D3C
+__osPfsGetInitData:
+
+base $70015ED0
+__osSiRawStartDma:
+
+base $70015F80
+__osContAddressCrc:
+
+base $70016030
+__osContDataCrc:
+
+base $70016100
+__osContRamWrite:
+
+base $700164B0
+__osContRamRead:
+
+base $70016860
+guAlignF:
+
+base $70016A20
+guAlign:
+
+base $70016A70
+guOrthoF:
+
+base $70016BC4
+guOrtho:
+
+base $70016C30
+coss:
+
+base $70016C60
+sins:
+
+base $70016CD0
+guTranslateF:
+
+base $70016D18
+guTranslate:
+
+base $70016D70
+_bcmp:
+
+base $70016E90
+guScaleF:
+
+base $70016EE4
+guScale:
+
+base $70016F30
+guLookAtReflectF:
+
+base $70017370
+guLookAtReflect:
+
+base $700173F0
+guLookAtF:
+
+base $700176A8
+guLookAt:
+
+base $70017720
+guRotateF:
+
+base $700178B4
+guRotate:
+
+base $70017910
+__d_to_ll:
+
+base $7001792C
+__f_to_ll:
+
+base $70017948
+__d_to_ull:
+
+base $700179E8
+__f_to_ull:
+
+base $70017A84
+__ll_to_d:
+
+base $70017A9C
+__ll_to_f:
+
+base $70017AB4
+__ull_to_d:
+
+base $70017AE8
+__ull_to_f:
+
+base $70017B20
+__osSetSR:
+
+base $70017B30
+__osGetSR:
+
+base $70017B40
+__osSiRawReadIo:
+
+base $70017B90
+__osSiRawWriteIo:
+
+base $70017BE0
+osPiRawReadIo:
+
+base $70017C40
+_bzero:
+
+base $70017CE0
+__osSetHWIntrRoutine:
+
+base $70017D30
+__osLeoInterrupt:
+
+base $700185F0
+__osDisableInt:
+
+base $70018610
+__osRestoreInt:
+
+base $70018630
+__osTimerServicesInit:
+
+base $700186BC
+__osTimerInterrupt:
+
+base $70018834
+__osSetTimerIntr:
+
+base $700188A8
+__osInsertTimer:
+
+base $70018A30
+osGetThreadPri:
+
+base $70018A50
+__osViInit:
+
+base $70018ED0
+__osSpGetStatus:
+
+base $70018EE0
+__osSpSetStatus:
+
+base $70018EF0
+__osSpSetPc:
+
+base $70018F30
+__osSpRawStartDma:
+
+base $70018FC0
+__osSpDeviceBusy:
+
+base $70018FF0
+__osDpDeviceBusy:
+
+base $70019020
+__osProbeTLB:
+
+base $700190E0
+alSynDelete:
+
+base $700190F0
+__osAiDeviceBusy:
+
+base $70019120
+alSaveNew:
+
+base $70019164
+alMainBusNew:
+
+base $700191B8
+alAuxBusNew:
+
+base $7001920C
+alResampleNew:
+
+base $70019294
+alLoadNew:
+
+base $7001933C
+alEnvmixerNew:
+
+base $700193E0
+init_lpfilter:
+
+base $70019480
+alFxNew:
+
+base $700198C0
+alSynAllocFX:
+
+base $70019960
+alMainBusParam:
+
+base $70019990
+alMainBusPull:
+
+base $70019AB0
+alLoadParam:
+
+base $70019C84
+alRaw16Pull:
+
+base $7001A15C
+alAdpcmPull:
+
+base $7001A600
+alResampleParam:
+
+base $7001A6C0
+def_7001A628:
+
+base $7001A6EC
+alResamplePull:
+
+base $7001A900
+_ldexpf:
+
+base $7001A928
+_frexpf:
+
+base $7001AA14
+alEnvmixerParam:
+
+base $7001B00C
+alEnvmixerPull:
+
+base $7001B3B8
+def_7001AF6C:
+
+base $7001B5F0
+alSaveParam:
+
+base $7001B624
+alSavePull:
+
+base $7001B6B0
+osJamMesg:
+
+base $7001B800
+osPiGetCmdQueue:
+
+base $7001B830
+__cosf:
+
+base $7001B9A0
+__sinf:
+
+base $7001BB60
+osDestroyThread:
+
+base $7001BC60
+__setInstChanState:
+
+base $7001BCB4
+__resetPerfChanState:
+
+base $7001BD34
+__initFromBank:
+
+base $7001BDF0
+__vsDelta:
+
+base $7001BE14
+__vsVol:
+
+base $7001BE9C
+__seqpReleaseVoice:
+
+base $7001BFC8
+__voiceNeedsNoteKill:
+
+base $7001C070
+__unmapVoice:
+
+base $7001C0E0
+__postNextSeqEvent:
+
+base $7001C234
+__vsPan:
+
+base $7001C27C
+__lookupVoice:
+
+base $7001C2F0
+__mapVoice:
+
+base $7001C360
+__lookupSoundQuick:
+
+base $7001CC64
+def_7001C98C:
+
+base $7001D24C
+def_7001CCD8:
+
+base $7001D2A0
+__seqpStopOsc:
+
+base $7001D3A4
+__initChanState:
+
+base $7001D414
+alSeqpNew:
+
+base $7001D5A0
+alSynStartVoiceParams:
+
+base $7001D690
+alCopy:
+
+base $7001D7D0
+_Litob:
+
+base $7001DFE0
+_Ldtob:
+
+base $7001E530
+osPiStartDma_1:
+
+base $7001E940
+def_7001E80C:
+
+base $7001E9C0
+__osSumcalc:
+
+base $7001EA1C
+__osIdCheckSum:
+
+base $7001EA84
+__osRepairPackId:
+
+base $7001EE9C
+__osCheckPackId:
+
+base $7001F034
+__osGetId:
+
+base $7001F290
+__osCheckId:
+
+base $7001F38C
+__osPfsRWInode:
+
+base $7001F6AC
+__osPfsSelectBank:
+
+base $7001F720
+__osSiDeviceBusy:
+
+base $7001F750
+guNormalize:
+
+base $7001F7E0
+sqrtf:
+
+base $7001F7F0
+osLeoDiskInit:
+
+base $7001F8E0
+__osSetCompare:
+
+base $7001F8F0
+alFilterNew:
+
+base $7001F910
+_doModFunc:
+
+base $7001F9B8
+_filterBuffer:
+
+base $7001FA70
+_saveBuffer:
+
+base $7001FBF8
+_loadBuffer:
+
+base $7001FD84
+_loadOutputBuffer:
+
+base $7001FFA8
+alFxParamHdl:
+
+base $70020204
+alFxParam:
+
+base $7002021C
+alFxPull:
+
+base $70020B40
+alSynSetPriority:
+
+base $70020B50
+lldiv:
+
+base $70020C50
+ldiv:
+
+base $70020CE0
+__osResetGlobalIntMask:
+
+base $70020D40
+osEPiRawWriteIo:
+
