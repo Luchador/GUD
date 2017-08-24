98f07e72cb76ecef99a7f372dc5c39841111d7d0
Larry <kholdfuzion@gmail.com>
2017-08-24 15:52:16 -0500

    finish messing with music for now

--- music/music.s
+++ music/music.s
@@ -1,3 +1,8 @@
+# assembler directives
+.set noat      # allow manual use of $at
+.set noreorder # don't insert nops after branches
+
+
 .section .music
 
 .global sfx.ctl
@@ -10,19 +15,19 @@ sfx.tbl:
  .incbin "music/sfx.tbl"
 sfx.tbl_end:
 
-.global music_sounds.ptr
-music_sounds.ptr:
+.global instruments.ctl
+instruments.ctl:
  .incbin "music/instruments.ctl"
-music_sounds.ptr_end:
+instruments.ctl_end:
 
-.global music_sounds.bnk
-music_sounds.bnk:
+.global instruments.tbl
+instruments.tbl:
  .incbin "music/instruments.tbl"
-music_sounds.bnk_end:
+instruments.tbl_end:
 
 .global number_music_samples
 number_music_samples:
- .half (music_table_music_data_end - music_table_music_data)/8
+ .half (table_music_data_end - number_music_samples)/8
  .half 0x0000
 number_music_samples_end:
 
@@ -33,229 +38,173 @@ number_music_samples_end:
  .half end_\name - \name
 .endm
 
-.global music_table_music_data
-music_table_music_data:
- music_table_entry music_no_music
- music_table_entry music_solo_death_abrev
- music_table_entry music_intro_eye
- music_table_entry music_train
- music_table_entry music_depot
- music_table_entry music_jungle_unused
- music_table_entry music_citadel
- music_table_entry music_facility
- music_table_entry music_control
- music_table_entry music_dam
- music_table_entry music_frigate
- music_table_entry music_archives
- music_table_entry music_silo
- music_table_entry music_jungle_perimeter_unused
- music_table_entry music_streets
- music_table_entry music_bunker1
- music_table_entry music_bunker2
- music_table_entry music_statue
- music_table_entry music_elevator_control
- music_table_entry music_cradle
- music_table_entry music_null1
- music_table_entry music_elevator_wc
- music_table_entry music_egyptian
- music_table_entry music_folders
- music_table_entry music_watchmusic
- music_table_entry music_aztec
- music_table_entry music_watercaverns
- music_table_entry music_deathsolo
- music_table_entry music_surface2
- music_table_entry music_trainx
- music_table_entry music_null2
- music_table_entry music_facilityx
- music_table_entry music_depotx
- music_table_entry music_controlx
- music_table_entry music_watercavernsx
- music_table_entry music_damx
- music_table_entry music_frigatex
- music_table_entry music_archivesx
- music_table_entry music_silox
- music_table_entry music_null3
- music_table_entry music_streetsx
- music_table_entry music_bunker1x
- music_table_entry music_bunker2x
- music_table_entry music_junglex
- music_table_entry music_nint_rare_logo
- music_table_entry music_statuex
- music_table_entry music_aztecx
- music_table_entry music_egyptianx
- music_table_entry music_cradlex
- music_table_entry music_cuba
- music_table_entry music_runway
- music_table_entry music_runway_plane
- music_table_entry music_surface2x
- music_table_entry music_windblowing
- music_table_entry music_multideath_alt
- music_table_entry music_jungle
- music_table_entry music_runwayx
- music_table_entry music_surface1
- music_table_entry music_multiplayerdeath
- music_table_entry music_surface1x
- music_table_entry music_surface2_ending
- music_table_entry music_statue_ending
- music_table_entry music_frigate_outro
-music_table_music_data_end:
+.global table_music_data
+table_music_data:
+ music_table_entry Mno_music
+ music_table_entry Msolo_death_abrev
+ music_table_entry Mintro_eye
+ music_table_entry Mtrain
+ music_table_entry Mdepot
+ music_table_entry Mjungle_unused
+ music_table_entry Mcitadel
+ music_table_entry Mfacility
+ music_table_entry Mcontrol
+ music_table_entry Mdam
+ music_table_entry Mfrigate
+ music_table_entry Marchives
+ music_table_entry Msilo
+ music_table_entry Mjungle_perimeter_unused
+ music_table_entry Mstreets
+ music_table_entry Mbunker1
+ music_table_entry Mbunker2
+ music_table_entry Mstatue
+ music_table_entry Melevator_control
+ music_table_entry Mcradle
+ music_table_entry Mnull1
+ music_table_entry Melevator_wc
+ music_table_entry Megyptian
+ music_table_entry Mfolders
+ music_table_entry Mwatchmusic
+ music_table_entry Maztec
+ music_table_entry Mwatercaverns
+ music_table_entry Mdeathsolo
+ music_table_entry Msurface2
+ music_table_entry Mtrainx
+ music_table_entry Mnull2
+ music_table_entry Mfacilityx
+ music_table_entry Mdepotx
+ music_table_entry Mcontrolx
+ music_table_entry Mwatercavernsx
+ music_table_entry Mdamx
+ music_table_entry Mfrigatex
+ music_table_entry Marchivesx
+ music_table_entry Msilox
+ music_table_entry Mnull3
+ music_table_entry Mstreetsx
+ music_table_entry Mbunker1x
+ music_table_entry Mbunker2x
+ music_table_entry Mjunglex
+ music_table_entry Mnint_rare_logo
+ music_table_entry Mstatuex
+ music_table_entry Maztecx
+ music_table_entry Megyptianx
+ music_table_entry Mcradlex
+ music_table_entry Mcuba
+ music_table_entry Mrunway
+ music_table_entry Mrunway_plane
+ music_table_entry Msurface2x
+ music_table_entry Mwindblowing
+ music_table_entry Mmultideath_alt
+ music_table_entry Mjungle
+ music_table_entry Mrunwayx
+ music_table_entry Msurface1
+ music_table_entry Mmultiplayerdeath
+ music_table_entry Msurface1x
+ music_table_entry Msurface2_ending
+ music_table_entry Mstatue_ending
+ music_table_entry Mfrigate_outro
+table_music_data_end:
 
 
 .macro music_file name
+.section .musiccompressed
+.global \name
 \name:
  .incbin "music\/\name\.rz"
 end_\name:
+
+.section .musicdecompressed
+.global d_\name
+d_\name:
+ .incbin "music\/\name\.bin"
+end_d_\name:
 .endm
+
 
 /*
 music_fileA is used for the entries that end with A....the A doesn't seem to be from compression
 */
 
 .macro music_fileA name
+.section .musiccompressed
+.global \name
 \name:
  .incbin "music\/\name\.rz"
  .byte 0xA
 end_\name:
-.endm
 
-music_file music_no_music
-music_fileA music_solo_death_abrev
-music_file music_intro_eye
-music_fileA music_train
-music_fileA music_depot
-music_file music_jungle_unused
-music_fileA music_citadel
-music_file music_facility
-music_file music_control
-music_fileA music_dam
-music_file music_frigate
-music_file music_archives
-music_file music_silo
-music_fileA music_jungle_perimeter_unused
-music_fileA music_streets
-music_fileA music_bunker1
-music_fileA music_bunker2
-music_file music_statue
-music_fileA music_elevator_control
-music_fileA music_cradle
-music_fileA music_null1
-music_fileA music_elevator_wc
-music_file music_egyptian
-music_file music_folders
-music_fileA music_watchmusic
-music_file music_aztec
-music_fileA music_watercaverns
-music_fileA music_deathsolo
-music_fileA music_surface2
-music_fileA music_trainx
-music_fileA music_null2
-music_file music_facilityx
-music_file music_depotx
-music_file music_controlx
-music_file music_watercavernsx
-music_fileA music_damx
-music_file music_frigatex
-music_fileA music_archivesx
-music_file music_silox
-music_fileA music_null3
-music_file music_streetsx
-music_file music_bunker1x
-music_fileA music_bunker2x
-music_fileA music_junglex
-music_file music_nint_rare_logo
-music_fileA music_statuex
-music_file music_aztecx
-music_file music_egyptianx
-music_fileA music_cradlex
-music_fileA music_cuba
-music_file music_runway
-music_file music_runway_plane
-music_file music_surface2x
-music_fileA music_windblowing
-music_file music_multideath_alt
-music_file music_jungle
-music_file music_runwayx
-music_fileA music_surface1
-music_file music_multiplayerdeath
-music_file music_surface1x
-music_fileA music_surface2_ending
-music_file music_statue_ending
-music_fileA music_frigate_outro
-
-.half 0
-.word 0
-
-.section .musicdiscard
-/*
-This sections sole purpose is to provide the decompressed size to the music table entry
-*/
-.macro d_music_file name
+.section .musicdecompressed
+.global d_\name
 d_\name:
  .incbin "music\/\name\.bin"
 end_d_\name:
 .endm
 
-d_music_file music_no_music
-d_music_file music_solo_death_abrev
-d_music_file music_intro_eye
-d_music_file music_train
-d_music_file music_depot
-d_music_file music_jungle_unused
-d_music_file music_citadel
-d_music_file music_facility
-d_music_file music_control
-d_music_file music_dam
-d_music_file music_frigate
-d_music_file music_archives
-d_music_file music_silo
-d_music_file music_jungle_perimeter_unused
-d_music_file music_streets
-d_music_file music_bunker1
-d_music_file music_bunker2
-d_music_file music_statue
-d_music_file music_elevator_control
-d_music_file music_cradle
-d_music_file music_null1
-d_music_file music_elevator_wc
-d_music_file music_egyptian
-d_music_file music_folders
-d_music_file music_watchmusic
-d_music_file music_aztec
-d_music_file music_watercaverns
-d_music_file music_deathsolo
-d_music_file music_surface2
-d_music_file music_trainx
-d_music_file music_null2
-d_music_file music_facilityx
-d_music_file music_depotx
-d_music_file music_controlx
-d_music_file music_watercavernsx
-d_music_file music_damx
-d_music_file music_frigatex
-d_music_file music_archivesx
-d_music_file music_silox
-d_music_file music_null3
-d_music_file music_streetsx
-d_music_file music_bunker1x
-d_music_file music_bunker2x
-d_music_file music_junglex
-d_music_file music_nint_rare_logo
-d_music_file music_statuex
-d_music_file music_aztecx
-d_music_file music_egyptianx
-d_music_file music_cradlex
-d_music_file music_cuba
-d_music_file music_runway
-d_music_file music_runway_plane
-d_music_file music_surface2x
-d_music_file music_windblowing
-d_music_file music_multideath_alt
-d_music_file music_jungle
-d_music_file music_runwayx
-d_music_file music_surface1
-d_music_file music_multiplayerdeath
-d_music_file music_surface1x
-d_music_file music_surface2_ending
-d_music_file music_statue_ending
-d_music_file music_frigate_outro
+music_file Mno_music
+music_fileA Msolo_death_abrev
+music_file Mintro_eye
+music_fileA Mtrain
+music_fileA Mdepot
+music_file Mjungle_unused
+music_fileA Mcitadel
+music_file Mfacility
+music_file Mcontrol
+music_fileA Mdam
+music_file Mfrigate
+music_file Marchives
+music_file Msilo
+music_fileA Mjungle_perimeter_unused
+music_fileA Mstreets
+music_fileA Mbunker1
+music_fileA Mbunker2
+music_file Mstatue
+music_fileA Melevator_control
+music_fileA Mcradle
+music_fileA Mnull1
+music_fileA Melevator_wc
+music_file Megyptian
+music_file Mfolders
+music_fileA Mwatchmusic
+music_file Maztec
+music_fileA Mwatercaverns
+music_fileA Mdeathsolo
+music_fileA Msurface2
+music_fileA Mtrainx
+music_fileA Mnull2
+music_file Mfacilityx
+music_file Mdepotx
+music_file Mcontrolx
+music_file Mwatercavernsx
+music_fileA Mdamx
+music_file Mfrigatex
+music_fileA Marchivesx
+music_file Msilox
+music_fileA Mnull3
+music_file Mstreetsx
+music_file Mbunker1x
+music_fileA Mbunker2x
+music_fileA Mjunglex
+music_file Mnint_rare_logo
+music_fileA Mstatuex
+music_file Maztecx
+music_file Megyptianx
+music_fileA Mcradlex
+music_fileA Mcuba
+music_file Mrunway
+music_file Mrunway_plane
+music_file Msurface2x
+music_fileA Mwindblowing
+music_file Mmultideath_alt
+music_file Mjungle
+music_file Mrunwayx
+music_fileA Msurface1
+music_file Mmultiplayerdeath
+music_file Msurface1x
+music_fileA Msurface2_ending
+music_file Mstatue_ending
+music_fileA Mfrigate_outro
+
+.half 0
+.word 0
+
 
