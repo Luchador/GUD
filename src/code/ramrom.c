908967f67dd70fa1f6f7d075331124ef40b6451f
kholdfuzion <kholdfuzion@gmail.com>
2018-10-08 07:33:33 -0500

    cleanup ramrom.c add ramrom.h

--- src/code/ramrom.c
+++ src/code/ramrom.c
@@ -1,4 +1,5 @@
 #include "ultra64.h"
+#include "ramrom.h"
 
 #define align_addr_zero(X) ((((X) + 0xF) | 0xF) ^ 0xF)
 #define align_addr_even(X) (((X) | 1) ^ 1)
@@ -7,47 +8,63 @@ OSIoMesg memoryMesgMB;
 OSMesg ptr_memorymsg;
 OSMesgQueue memoryMesgQueue;
 
-
-void creates_a_message_queue_for_memory(void) {
+/* external */
+/* romCreateMesgQueue */
+/* creates a message queue */
+void romCreateMesgQueue(void) {
     osCreateMesgQueue(&memoryMesgQueue, &ptr_memorymsg, 1);
 }
 
 
-void read_bytes_from_hw_to_rdram(void *target, void *source, u32 size) {
+/* doRomCopy*/
+/* invalidate cache and do pi dma */
+void doRomCopy(void *target, void *source, u32 size) {
     osInvalDCache(target, size);
     osPiStartDma(&memoryMesgMB, 0, 0, source, target, size, &memoryMesgQueue);
 }
 
-void receive_memoryMesg(void) {
+/* romReceiveMesg */
+/* receives a message queue */
+void romReceiveMesg(void) {
     osRecvMesg(&memoryMesgQueue, 0, 1);
 }
 
-void load_bytes_from_hw_to_rdram(void *target, void *source, u32 size) {
-    read_bytes_from_hw_to_rdram(target, source, size);
-    receive_memoryMesg();
+/* external */
+/* romCopy */
+/* copy from rom to ram */
+void romCopy(void *target, void *source, u32 size) {
+    doRomCopy(target, source, size);
+    romReceiveMesg();
 }
 
-s32 load_ramrom_file(void *target, void *source, s32 length) {
-    s32 sp18;
-    s32 *sp1C;
-    s32 *temp_t8;
-    s32 *temp_v1;
+/* external */
+/* romCopyAligned */
+/* aligns data, does a romCopy(), then returns aligned pointer to target */
+s32 romCopyAligned(void *target, void *source, s32 length) {
+    s32 target_offset;
+    s32 *target_aligned;
+    s32 *source_aligned;
+    s32 *source_offset;
 
-    // Node 0
-    temp_t8 = align_addr_even((s32)source);
-    temp_v1 = (s32)source - (s32)temp_t8;
-    sp1C = align_addr_zero((s32)target);
-    sp18 = temp_v1;
-    load_bytes_from_hw_to_rdram(sp1C, temp_t8, align_addr_zero((s32)temp_v1 + length));
-    return ((s32)sp1C + sp18);
+    source_aligned = align_addr_even((s32)source);
+    source_offset = (s32)source - (s32)source_aligned;
+    target_aligned = align_addr_zero((s32)target);
+    target_offset = source_offset;
+    romCopy(target_aligned, source_aligned, align_addr_zero((s32)source_offset + length));
+    return ((s32)target_aligned + target_offset);
 }
 
-void write_rdram_to_hw(void *source, void *target, u32 size) {
+/* doRomWrite*/
+/* actually writes to rom (buffer on Indy) */
+void doRomWrite(void *source, void *target, u32 size) {
     osWritebackDCache(source, size);
     osPiStartDma(&memoryMesgMB, 0, 1, target, source, size, &memoryMesgQueue);
 }
 
-void load_rdram_to_hw(void *source, void *target, u32 size) {
-    write_rdram_to_hw(source, target, size);
-    receive_memoryMesg();
+/* external */
+/* romWrite */
+/* let's write to the rom (buffer on Indy) */
+void romWrite(void *source, void *target, u32 size) {
+    doRomWrite(source, target, size);
+    romReceiveMesg();
 }
