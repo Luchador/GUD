5cf13cf15d9f64d2a894a719ea5ade79e88d5d41
Trevor Leeming <46264947+Trevor89@users.noreply.github.com>
2019-04-14 18:46:51 +0100

    named ram add osViClock, fixed wrong name osTvType

--- lib/src/__osViInit.c
+++ lib/src/__osViInit.c
@@ -8,8 +8,8 @@ extern u32 osTvType;
 OSViContext D_803348B0[2] = {0};
 OSViContext *__osViCurr = &D_803348B0[0];
 OSViContext *__osViNext = &D_803348B0[1];
-u32 osViClock = TV_TYPE_NTSC;
-u32 D_8033491C = 0x02E6D354;
+u32 osTvType = TV_TYPE_NTSC;
+u32 osViClock = 0x02E6D354;
 
 extern OSViMode D_80334990;
 extern OSViMode D_803349E0;
@@ -17,7 +17,7 @@ void __osViInit(void)
 {
 //#ifdef VERSION_JP
 #ifdef VERSION_US
-    osViClock = osTvType;
+    osTvType = osTvType;
 #endif
     bzero(D_803348B0, sizeof(D_803348B0));
     __osViCurr = &D_803348B0[0];
@@ -25,21 +25,21 @@ void __osViInit(void)
     __osViNext->retraceCount = 1;
     __osViCurr->retraceCount = 1;
 #ifdef VERSION_JP
-    if (osViClock != TV_TYPE_PAL)
+    if (osTvType != TV_TYPE_PAL)
 #else
-    if (osViClock == TV_TYPE_NTSC)
+    if (osTvType == TV_TYPE_NTSC)
 #endif
     {
         __osViNext->unk08 = &D_80334990;
-        D_8033491C = 0x02E6D354;
+        osViClock = 0x02E6D354;
     }
     else
     {
         __osViNext->unk08 = &D_803349E0;
 #ifdef VERSION_JP
-        D_8033491C = 0x02F5B2D2;
+        osViClock = 0x02F5B2D2;
 #else
-        D_8033491C = 0x02E6025C;
+        osViClock = 0x02E6025C;
 #endif
     }
     __osViNext->unk00 = 32;
