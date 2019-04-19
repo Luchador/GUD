1cc2f74f8be4832612353619514a8242d0e6196d
KholdFuzion <kholdfuzion@gmail.com>
2019-04-19 07:46:07 -0500

    eeprom.c data

--- src/game/eeprom.h
+++ src/game/eeprom.h
@@ -1,7 +1,7 @@
 #ifndef _EEPROM_H_
 #define _EEPROM_H_
 #include "ultra64.h"
-
+//i am not "correct", i need proper fields for times again
 struct save_data
 {
   int chksum1;
@@ -15,73 +15,24 @@ struct save_data
   char unlocked_cheats_1;
   char unlocked_cheats_2;
   char unlocked_cheats_3;
-  char field_11;
-  unsigned agent_dam_time :10;
-  unsigned agent_facility_time :10;
-  unsigned agent_runway_time :10;
-  unsigned agent_surface1_time :10;
-  unsigned agent_bunker1_time :10;
-  unsigned agent_silo_time :10;
-  unsigned agent_frigate_time :10;
-  unsigned agent_surface2_time :10;
-  unsigned agent_bunker2_time :10;
-  unsigned agent_statue_time :10;
-  unsigned agent_archives_time :10;
-  unsigned agent_streets_time :10;
-  unsigned agent_depot_time :10;
-  unsigned agent_train_time :10;
-  unsigned agent_jungle_time :10;
-  unsigned agent_control_time :10;
-  unsigned agent_caverns_time :10;
-  unsigned agent_cradle_time :10;
-  unsigned agent_aztec_time :10;
-  unsigned agent_egyptian_time :10;
+  char padding;
+    u8 time_dam_agent[5];
+    u8 field_0x17[5];
+    u8 field_0x1c[5];
+    u8 field_0x21[5];
+    u8 field_0x26[5];
+    u8 field_0x2b[5];
+    u8 field_0x30[5];
+    u8 field_0x35[5];
+    u8 field_0x3a[5];
+    u8 field_0x3f[5];
+    u8 field_0x44[5];
+    u8 field_0x49[5];
+    u8 field_0x4e[5];
+    u8 field_0x53[5];
+    u8 field_0x58[5];
+    u8 field_0x5d[3];
 
-  unsigned secretagent_dam_time :10;
-  unsigned secretagent_facility_time :10;
-  unsigned secretagent_runway_time :10;
-  unsigned secretagent_surface1_time :10;
-  unsigned secretagent_bunker1_time :10;
-  unsigned secretagent_silo_time :10;
-  unsigned secretagent_frigate_time :10;
-  unsigned secretagent_surface2_time :10;
-  unsigned secretagent_bunker2_time :10;
-  unsigned secretagent_statue_time :10;
-  unsigned secretagent_archives_time :10;
-  unsigned secretagent_streets_time :10;
-  unsigned secretagent_depot_time :10;
-  unsigned secretagent_train_time :10;
-  unsigned secretagent_jungle_time :10;
-  unsigned secretagent_control_time :10;
-  unsigned secretagent_caverns_time :10;
-  unsigned secretagent_cradle_time :10;
-  unsigned secretagent_aztec_time :10;
-  unsigned secretagent_egyptian_time :10;
-
-  unsigned agent00_dam_time :10;
-  unsigned agent00_facility_time :10;
-  unsigned agent00_runway_time :10;
-  unsigned agent00_surface1_time :10;
-  unsigned agent00_bunker1_time :10;
-  unsigned agent00_silo_time :10;
-  unsigned agent00_frigate_time :10;
-  unsigned agent00_surface2_time :10;
-  unsigned agent00_bunker2_time :10;
-  unsigned agent00_statue_time :10;
-  unsigned agent00_archives_time :10;
-  unsigned agent00_streets_time :10;
-  unsigned agent00_depot_time :10;
-  unsigned agent00_train_time :10;
-  unsigned agent00_jungle_time :10;
-  unsigned agent00_control_time :10;
-  unsigned agent00_caverns_time :10;
-  unsigned agent00_cradle_time :10;
-  unsigned agent00_aztec_time :10;
-  unsigned agent00_egyptian_time :10;
-
-  char unused1;
-  char unused2;
-  char unused3;
 };
 
 #endif
