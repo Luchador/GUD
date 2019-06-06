e7ee1c2896c9ff6b3527a9fd6296f9d4b2b91412
Carnivorous <38839485+carnivoroussociety@users.noreply.github.com>
2019-06-06 03:46:31 +0000

    Updated UbriefarchZ

--- assets/obseg/brief/UbriefarchZ.c
+++ assets/obseg/brief/UbriefarchZ.c
@@ -8,9 +8,9 @@ struct BriefStruct UbriefarchZ = {
         0x0803               /* Moneypenny */
     },
     {                        /* Objectives (Text and Difficult) */
-        {0x0804, DIFF_A},    /* Escape interrogation room */
+        {0x0804, DIFF_A},    /* Escape from interrogation room */
         {0x0805, DIFF_A},    /* Find Natalya */
-        {0x0806, DIFF_SA},   /* Recover heli black box */
+        {0x0806, DIFF_SA},   /* Recover helicopter black box */
         {0x0807, DIFF_A},    /* Escape with Natalya */
         {0x0000, 0x0000},    /* Not assigned */
         {0x0000, 0x0000},    /* Not assigned */
