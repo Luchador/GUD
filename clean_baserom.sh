723626d6b255a59657f73c8cccbdb48f39b8a99c
KholdFuzion <kholdfuzion@gmail.com>
2019-06-10 08:13:09 -0500

    ok should be ready enough to push

--- clean_baserom.sh
+++ clean_baserom.sh
@@ -3,16 +3,12 @@ echo "cleaning up files created by extract_baserom.sh"
 true="1"
 while IFS=, read -r offset size name compressed extract
 do
-    if [ "$extract" == "$true" ]; then
-        if [ "$compressed" == "$true" ]; then
-            echo "removing $name.bin"
-            rm -f $name.bin
-        else
-        echo "removing $name"
-            rm -f $name
-        fi
+    if [ "$compressed" == "$true" ]; then
+        echo "removing $name.bin"
+        rm -f $name.bin
     else
-        echo "skip $name"
+    echo "removing $name"
+        rm -f $name
     fi
 done < filelist.u.csv
 
