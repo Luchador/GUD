6ce55f16371d59332aa9a3ce387f886f60d7b682
Coockie1173 <rudjerud@hotmail.com>
2019-09-21 16:58:02 +0200

    Update chraicommands.h
    All one byte commands should (in theory) be added.
    If the LF -> CLRF line ending breaks stuff, remove the commit

--- src/game/chraicommands.h
+++ src/game/chraicommands.h
@@ -227,12 +227,84 @@
         guard_animation_looks_around_self_ID,
 
 /*=============================================================================
+// name: guard_jogs_to_predefined
+// command id: 1D
+//=============================================================================
+// info: makes the guard jog to predefined pad 2328
+//===========================================================================*/
+#define guard_jogs_to_predefined_ID 0x1D
+#define guard_jogs_to_predefined_LENGTH 0x01
+#define guard_jogs_to_predefined \
+        guard_jogs_to_predefined_ID,
+
+
+/*=============================================================================
+// name: guard_surrenders
+// command id: 21
+//=============================================================================
+// info: makes a guard surrender and drop all his objects
+//===========================================================================*/
+#define guard_surrenders_ID 0x21
+#define guard_surrenders_LENGTH 0x01
+#define guard_surrenders \
+        guard_surrenders_ID,
+
+/*=============================================================================
+// name: guard_move_disappear
+// command id: 22
+//=============================================================================
+// info: 
+//===========================================================================*/
+#define guard_move_disappear_ID 0x22
+#define guard_move_disappear_LENGTH 0x01
+#define guard_move_disappear \
+        guard_move_disappear_ID,
+
+
+/*=============================================================================
+// name: activate_alarm
+// command id: 25
+//=============================================================================
+// info: activates alarm
+//===========================================================================*/
+
+#define activate_alarm_ID 0x25
+#define activate_alarm_LENGTH 0x01
+#define activate_alarm \
+        activate_alarm_ID,
+
+/*=============================================================================
+// name: deactivate_alarm
+// command id: 26
+//=============================================================================
+// info: deactivates alarm
+//===========================================================================*/
+
+#define deactivate_alarm_ID 0x26
+#define deactivate_alarm_LENGTH 0x01
+#define deactivate_alarm \
+        deactivate_alarm_ID,
+
+/*=============================================================================
+// name: seed_random_byte
+// command id: 33
+//=============================================================================
+// info: seeds random byte
+//===========================================================================*/
+
+#define seed_random_byte_ID 0x33
+#define seed_random_byte_LENGTH 0x01
+#define seed_random_byte \
+        seed_random_byte_ID,
+
+/*=============================================================================
 // name: chr_in_room_with_pad
 // command id: 54
 //=============================================================================
 // info: if chr id in same room with pad, goto label
 //===========================================================================*/
 #define chr_in_room_with_pad_ID 0x54
+#define chr_in_room_with_pad_LENGTH 0x05
 #define chr_in_room_with_pad(chr_id, pad, label) \
         chr_in_room_with_pad_ID, \
         chr_id, \
@@ -246,9 +318,275 @@
 // info: if bond in same room with pad, goto label
 //===========================================================================*/
 #define bond_in_room_with_pad_ID 0x55
+#define bond_in_room_with_pad_LENGTH 0x04
 #define bond_in_room_with_pad(pad, label) \
         bond_in_room_with_pad_ID, \
         chararray16(pad), \
         label,
+
+/*=============================================================================
+// name: reset_cycle_counter_and_enable
+// command id: AE
+//=============================================================================
+// info: resets the cycle counter and enables it
+//===========================================================================*/
+#define reset_cycle_counter_and_enable_ID 0xAE
+#define reset_cycle_counter_and_enable_LENGTH 0x01
+#define reset_cycle_counter_and_enable \
+        reset_cycle_counter_and_enable_ID,
+
+/*=============================================================================
+// name: reset_cycle_counter
+// command id: AF
+//=============================================================================
+// info: resets the cycle counter
+//===========================================================================*/
+#define reset_cycle_counter_ID 0xAF
+#define reset_cycle_counter_LENGTH 0x01
+#define reset_cycle_counter \
+        reset_cycle_counter_ID,
+
+/*=============================================================================
+// name: disable_cycle_counter
+// command id: B0
+//=============================================================================
+// info: stops the cycle counter
+//===========================================================================*/
+#define disable_cycle_counter_ID 0xB0
+#define disable_cycle_counter_LENGTH 0x01
+#define disable_cycle_counter \
+        disable_cycle_counter_ID,
+
+/*=============================================================================
+// name: enable_cycle_counter
+// command id: B1
+//=============================================================================
+// info: starts the cycle counter
+//===========================================================================*/
+#define enable_cycle_counter_ID 0xB1
+#define enable_cycle_counter_LENGTH 0x01
+#define enable_cycle_counter \
+        enable_cycle_counter_ID
+
+/*=============================================================================
+// name: show_timer
+// command id: B5
+//=============================================================================
+// info: shows the timer
+//===========================================================================*/
+#define show_timer_ID 0xB5
+#define show_timer_LENGTH 0x01
+#define show_timer \
+        show_timer_ID,
+
+/*=============================================================================
+// name: hide_timer
+// command id: B6
+//=============================================================================
+// info: hides the timer
+//===========================================================================*/
+#define hide_timer_ID 0xB6
+#define hide_timer_LENGTH 0x01
+#define hide_timer \
+        hide_timer_ID,
+
+/*=============================================================================
+// name: stop_timer
+// command id: B8
+//=============================================================================
+// info: stops the timer
+//===========================================================================*/
+#define stop_timer_ID 0xB8
+#define stop_timer_LENGTH 0x01
+#define stop_timer \
+        stop_timer_ID,
+
+/*=============================================================================
+// name: start_timer
+// command id: B9
+//=============================================================================
+// info: starts the timer
+//===========================================================================*/
+#define start_timer_ID 0xB9
+#define start_timer_LENGTH 0x01
+#define start_timer \
+        start_timer_ID,
+
+/*=============================================================================
+// name: exit_level
+// command id: D2
+//=============================================================================
+// info: exits the level
+//===========================================================================*/
+#define exit_level_ID 0xD2
+#define exit_level_LENGTH 0x01
+#define exit_level \
+        exit_level_ID,
+
+/*=============================================================================
+// name: return_from_camera_scene
+// command id: D3
+//=============================================================================
+// info: 
+//===========================================================================*/
+#define return_from_camera_scene_ID 0xD3
+#define return_from_camera_scene_LENGTH 0x01
+#define return_from_camera_scene \
+        return_from_camera_scene_ID,
+
+/*=============================================================================
+// name: enable_all_on_screen_displays
+// command id: D8
+//=============================================================================
+// info:
+//===========================================================================*/
+#define enable_all_on_screen_displays_ID 0xD8
+#define enable_all_on_screen_displays_LENGTH 0x01
+#define enable_all_on_screen_displays \
+        enable_all_on_screen_displays_ID,
+
+/*=============================================================================
+// name: fade_out
+// command id: DA
+//=============================================================================
+// info: fades the screen out
+//===========================================================================*/
+#define fade_out_ID 0xDA
+#define fade_out_LENGTH 0x01
+#define fade_out \
+        fade_out_ID,
+
+/*=============================================================================
+// name: fade_in
+// command id: DB
+//=============================================================================
+// info: fades the screen in
+//===========================================================================*/
+#define fade_in_ID 0xDB
+#define fade_in_LENGTH 0x01
+#define fade_in \
+        fade_in_ID,
+
+/*=============================================================================
+// name: hide_all_chr
+// command id: DD
+//=============================================================================
+// info: 
+//===========================================================================*/
+#define hide_all_chr_ID 0xDD
+#define hide_all_chr_LENGTH 0x01
+#define hide_all_chr \
+        hide_all_chr_ID,
+
+/*=============================================================================
+// name: show_all_chr
+// command id: DE
+//=============================================================================
+// info:
+//===========================================================================*/
+#define show_all_chr_ID 0xDE
+#define show_all_chr_LENGTH 0x01
+#define show_all_chr \
+        show_all_chr_ID,
+
+/*=============================================================================
+// name: sky_switch_instant
+// command id: E9
+//=============================================================================
+// info: instantly switches sky bank
+//===========================================================================*/
+#define sky_switch_instant_ID 0xE9
+#define sky_switch_instant_LENGTH 0x01
+#define sky_switch_instant \
+        sky_switch_instant_ID,
+
+/*=============================================================================
+// name: stop_game_time
+// command id: EA
+//=============================================================================
+// info: stops the mission time
+//===========================================================================*/
+#define stop_game_time_ID 0xEA
+#define stop_game_time_LENGTH 0x01
+#define stop_game_time \
+        stop_game_time_ID,
+
+/*=============================================================================
+// name: bond_disable_pickups_and_damage
+// command id: EC
+//=============================================================================
+// info: Use this for level exit trigger, so Bond cannot die during exit cutscene.
+//===========================================================================*/
+#define bond_disable_pickups_and_damage_ID 0xEC
+#define bond_disable_pickups_and_damage_LENGTH 0x01
+#define bond_disable_pickups_and_damage \
+        bond_disable_pickups_and_damage_ID,
+
+/*=============================================================================
+// name: hide_first_person_display
+// command id: ED
+//=============================================================================
+// info: 
+//===========================================================================*/
+#define hide_first_person_display_ID 0xED
+#define hide_first_person_display_LENGTH 0x01
+#define hide_first_person_display \
+        hide_first_person_display_ID,
+
+/*=============================================================================
+// name: trigger_credits
+// command id: EF
+//=============================================================================
+// info:
+//===========================================================================*/
+#define trigger_credits_ID 0xEF
+#define trigger_credits_LENGTH 0x01
+#define trigger_credits \
+        trigger_credits_ID,
+
+/*=============================================================================
+// name: trigger_explosions
+// command id: F6
+//=============================================================================
+// info: triggers explosions around the player
+//===========================================================================*/
+#define trigger_explosions_ID 0xF6
+#define trigger_explosions_LENGTH 0x01
+#define trigger_explosions \
+        trigger_explosions_ID,
+
+/*=============================================================================
+// name: set_killed_in_action
+// command id: F9
+//=============================================================================
+// info: sets player state to KIA. Automatic mission failure
+//===========================================================================*/
+#define set_killed_in_action_ID 0xF9
+#define set_killed_in_action_LENGTH 0x01
+#define set_killed_in_action \
+        set_killed_in_action_ID,
+
+/*=============================================================================
+// name: guard_raises_arms
+// command id: FA
+//=============================================================================
+// info: makes guard raise his arms for half a second
+//===========================================================================*/
+#define guard_raises_arms_ID 0xFA
+#define guard_raises_arms_LENGTH 0x01
+#define guard_raises_arms \
+        guard_raises_arms_ID,
+
+/*=============================================================================
+// name: sky_switch_transition
+// command id: FB
+//=============================================================================
+// info: fades between sky banks
+//===========================================================================*/
+#define sky_switch_transition_ID 0xFB
+#define sky_switch_transition_LENGTH 0x01
+#define sky_switch_transition \
+        sky_switch_transition_ID,
+
 
 #endif
