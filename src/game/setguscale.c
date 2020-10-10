c7e40e623ff46e898a44f0c9c91971d54a95e0d6
thespider64 <spidersixtyfour@gmail.com>
2020-10-10 13:10:27 -0300

    Inline floats

--- src/game/setguscale.c
+++ src/game/setguscale.c
@@ -2,8 +2,6 @@
 #include "game/unk_09C250.h"
 
 void set_gu_scale(void) {
-    float default_gu_scale = 0.1f;
-
-    guScale((Mtx *)&dword_CODE_bss_8007A100, default_gu_scale, default_gu_scale, default_gu_scale);
+    guScale((Mtx *)&dword_CODE_bss_8007A100, 0.1f, 0.1f, 0.1f);
 }
 
