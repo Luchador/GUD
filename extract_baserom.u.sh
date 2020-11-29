d0508e7170ae18ef792c803f1145cc34c322007f
Trevor Leeming <46264947+Trevor89@users.noreply.github.com>
2020-11-29 14:42:19 +0000

    Allowed relocatable baserom image

--- extract_baserom.u.sh
+++ extract_baserom.u.sh
@@ -1,29 +1,60 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    DOALL="1"
-    echo "Processing Everything"
+
+#set defaults
+DOALL="1"
+BASEROM="baserom.u.z64"
+
+if [ -z "$2" ]; then
+    if [ -z "$1" ]; then
+        #No Args
+        echo "Processing Everything"
+    elif [ "$1" = "files" ] || [ "$1" = "images" ]; then
+        #Arg1 is "files" or "images"
+        DOALL="0"
+        echo "Processing $1 Only"
+    else
+        #Arg1 is ROM
+        BASEROM=$1
+        echo "Processing Everything using $BASEROM"
+    fi
+else
+    BASEROM=$2
+    if [ "$1" = "files" ] || [ "$1" != "images" ]; then
+        #Arg1 is "files" or "images
+        DOALL="0"
+        echo "Processing $1 Only using $BASEROM"
+    else
+        echo "Processing Everything using $BASEROM"
+    fi
 fi
 
+
 true="1"
-mkdir assets assets/font assets/images assets/images/split assets/music assets/ramrom assets/obseg assets/obseg/bg assets/obseg/brief assets/obseg/chr assets/obseg/gun assets/obseg/prop assets/obseg/setup assets/obseg/setup/u assets/obseg/stan assets/obseg/text assets/obseg/text/u
+
+DIRs='assets assets/font assets/images assets/images/split assets/music assets/ramrom assets/obseg assets/obseg/bg assets/obseg/brief assets/obseg/chr assets/obseg/gun assets/obseg/prop assets/obseg/setup assets/obseg/setup/u assets/obseg/stan assets/obseg/text assets/obseg/text/u'
+
+for DIR in $DIRs
+do
+    # if dir not exist, make
+    if [ ! -d $DIR ]; then
+        mkdir $DIR
+    fi
+done
+
 
 if [ "$DOALL" == "1" ] || [ $1 == 'files' ]; then
     echo "Processing Files"
+      make -C tools/extractor clean && make -C tools/extractor
     if [ -f tools/extractor/extractor ];
     then
-        make -C tools/extractor clean
-    fi
-    make -C tools/extractor
-    if [ -f tools/extractor/extractor ];
-    then
-        tools/extractor/extractor baserom.u.z64 filelist.u.csv
+        tools/extractor/extractor "$BASEROM" filelist.u.csv
     else
         while IFS=, read -r offset size name compressed extract
         do
             if [ "$extract" == "$true" ]; then
                 if [ "$compressed" == "$true" ]; then
                     echo "Extracting compressed $name, $size bytes..."
-                    dd bs=1 skip=$offset count=$size if=baserom.u.z64 of=$name status=none
+                    dd bs=1 skip=$offset count=$size if="$BASEROM" of=$name status=none
                      # Add the gZip Header to a new file using the name given in command
                     echo -n -e \\x1F\\x8B\\x08\\x00\\x00\\x00\\x00\\x00\\x02\\x03 > $name.temp
                      # Add the contents of the compressed file minus the 1172 to the new file
@@ -37,7 +68,7 @@ if [ "$DOALL" == "1" ] || [ $1 == 'files' ]; then
                     echo "Successfully Decompressed $name"
                 else
                     echo "Extracting uncompressed $name, $size bytes..."
-                    dd bs=1 skip=$offset count=$size if=baserom.u.z64 of=$name status=none
+                    dd bs=1 skip=$offset count=$size if="$BASEROM" of=$name status=none
                     echo "Successfully Extracted $name"
                 fi
             else
@@ -53,19 +84,15 @@ fi
 
 if [ "$DOALL" == "1" ] || [ $1 == 'images' ]; then
     echo "Processing Images"
+    make -C tools/extractor clean && make -C tools/extractor
     if [ -f tools/extractor/extractor ];
     then
-        make -C tools/extractor clean
-    fi
-    make -C tools/extractor
-    if [ -f tools/extractor/extractor ];
-    then
-        tools/extractor/extractor baserom.u.z64 imagelist.u.csv
+        tools/extractor/extractor "$BASEROM" imagelist.u.csv
     else
         while IFS=, read -r offset size name
         do
             echo "Extracting $name, $size bytes..."
-            dd bs=1 skip=$offset count=$size if=baserom.u.z64 of=$name status=none
+            dd bs=1 skip=$offset count=$size if="$BASEROM" of=$name status=none
             echo "Successfully Extracted $name"
         done < imagelist.u.csv
     fi
