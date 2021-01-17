adb320d0209f853a82fd36e1ab34db96c32f164f
Wyst3r <wyst3r@gmail.com>
2021-01-17 20:24:48 +0100

    decompile _MakeMotorData

--- src/motor.c
+++ src/motor.c
@@ -1,15 +1,13 @@
 #include "ultra64.h"
 #include "libultra/controller.h"
-//0x800655a0
+
 OSPifRam  _MotorStopData[MAXCONTROLLERS];
-//800656a0
 OSPifRam  _MotorStartData[MAXCONTROLLERS];
-//800657a0 
 u8 _motorstopbuf[32];
-//800657c0
 u8 _motorstartbuf[32];
 
-s32 osMotorStop(OSPfs *pfs) {
+s32 osMotorStop(OSPfs *pfs)
+{
     int i;
     s32 ret;
     u8 *ptr;
@@ -71,112 +69,35 @@ s32 osMotorStart(OSPfs *pfs)
     return ret;
 }
 
-#ifdef NONMATCHING
-void _MakeMotorData(int channel,u16 address,u8 *buffer,OSPifRam *mdata)
+void _MakeMotorData(int channel, u16 address, u8 *buffer, OSPifRam *mdata)
 {
+    u8 *ptr;
+    __OSContRamReadFormat ramreadformat;
+    int i;
 
+    ptr = (u8 *)mdata->ramarray;
+    for (i = 0; i < ARRLEN(mdata->ramarray); i++)
+        mdata->ramarray[i] = 0;
+    mdata->pifstatus = CONT_CMD_EXE;
+    ramreadformat.dummy = CONT_CMD_NOP;
+    ramreadformat.txsize = CONT_CMD_WRITE_MEMPACK_TX;
+    ramreadformat.rxsize = CONT_CMD_WRITE_MEMPACK_RX;
+    ramreadformat.cmd = CONT_CMD_WRITE_MEMPACK;
+    ramreadformat.address = (address << 0x5) | __osContAddressCrc(address);
+    ramreadformat.datacrc = CONT_CMD_NOP;
+    for (i = 0; i < ARRLEN(ramreadformat.data); i++)
+        ramreadformat.data[i] = *buffer++;
+    if (channel != 0)
+    {
+        for (i = 0; i < channel; i++)
+        {
+            *ptr++ = 0;
+        }
+    }
+    *(__OSContRamReadFormat *)ptr = ramreadformat;
+    ptr += sizeof(__OSContRamReadFormat);
+    ptr[0] = CONT_CMD_END;
 }
-#else
-GLOBAL_ASM(
-.text
-glabel _MakeMotorData
-/* 00D7DC 7000CBDC 27BDFFA8 */  addiu $sp, $sp, -0x58
-/* 00D7E0 7000CBE0 AFB00018 */  sw    $s0, 0x18($sp)
-/* 00D7E4 7000CBE4 00808025 */  move  $s0, $a0
-/* 00D7E8 7000CBE8 AFBF001C */  sw    $ra, 0x1c($sp)
-/* 00D7EC 7000CBEC AFA5005C */  sw    $a1, 0x5c($sp)
-/* 00D7F0 7000CBF0 00E01825 */  move  $v1, $a3
-/* 00D7F4 7000CBF4 00004025 */  move  $t0, $zero
-/* 00D7F8 7000CBF8 00E01025 */  move  $v0, $a3
-.L7000CBFC:
-/* 00D7FC 7000CBFC 25080001 */  addiu $t0, $t0, 1
-/* 00D800 7000CC00 2901000F */  slti  $at, $t0, 0xf
-/* 00D804 7000CC04 24420004 */  addiu $v0, $v0, 4
-/* 00D808 7000CC08 1420FFFC */  bnez  $at, .L7000CBFC
-/* 00D80C 7000CC0C AC40FFFC */   sw    $zero, -4($v0)
-/* 00D810 7000CC10 240E0001 */  li    $t6, 1
-/* 00D814 7000CC14 ACEE003C */  sw    $t6, 0x3c($a3)
-/* 00D818 7000CC18 240F00FF */  li    $t7, 255
-/* 00D81C 7000CC1C 24180023 */  li    $t8, 35
-/* 00D820 7000CC20 24190001 */  li    $t9, 1
-/* 00D824 7000CC24 24090003 */  li    $t1, 3
-/* 00D828 7000CC28 A3AF002C */  sb    $t7, 0x2c($sp)
-/* 00D82C 7000CC2C A3B8002D */  sb    $t8, 0x2d($sp)
-/* 00D830 7000CC30 A3B9002E */  sb    $t9, 0x2e($sp)
-/* 00D834 7000CC34 A3A9002F */  sb    $t1, 0x2f($sp)
-/* 00D838 7000CC38 AFA60060 */  sw    $a2, 0x60($sp)
-/* 00D83C 7000CC3C AFA30054 */  sw    $v1, 0x54($sp)
-/* 00D840 7000CC40 0C0057E0 */  jal   __osContAddressCrc
-/* 00D844 7000CC44 97A4005E */   lhu   $a0, 0x5e($sp)
-/* 00D848 7000CC48 97AA005E */  lhu   $t2, 0x5e($sp)
-/* 00D84C 7000CC4C 240D00FF */  li    $t5, 255
-/* 00D850 7000CC50 8FA30054 */  lw    $v1, 0x54($sp)
-/* 00D854 7000CC54 000A5940 */  sll   $t3, $t2, 5
-/* 00D858 7000CC58 004B6025 */  or    $t4, $v0, $t3
-/* 00D85C 7000CC5C A7AC0030 */  sh    $t4, 0x30($sp)
-/* 00D860 7000CC60 8FA60060 */  lw    $a2, 0x60($sp)
-/* 00D864 7000CC64 A3AD0052 */  sb    $t5, 0x52($sp)
-/* 00D868 7000CC68 27A2004C */  addiu $v0, $sp, 0x4c
-/* 00D86C 7000CC6C 27A4002C */  addiu $a0, $sp, 0x2c
-.L7000CC70:
-/* 00D870 7000CC70 90CE0000 */  lbu   $t6, ($a2)
-/* 00D874 7000CC74 24840001 */  addiu $a0, $a0, 1
-/* 00D878 7000CC78 0082082B */  sltu  $at, $a0, $v0
-/* 00D87C 7000CC7C 24C60001 */  addiu $a2, $a2, 1
-/* 00D880 7000CC80 1420FFFB */  bnez  $at, .L7000CC70
-/* 00D884 7000CC84 A08E0005 */   sb    $t6, 5($a0)
-/* 00D888 7000CC88 12000012 */  beqz  $s0, .L7000CCD4
-/* 00D88C 7000CC8C 27AF002C */   addiu $t7, $sp, 0x2c
-/* 00D890 7000CC90 1A000010 */  blez  $s0, .L7000CCD4
-/* 00D894 7000CC94 00004025 */   move  $t0, $zero
-/* 00D898 7000CC98 32040003 */  andi  $a0, $s0, 3
-/* 00D89C 7000CC9C 10800006 */  beqz  $a0, .L7000CCB8
-/* 00D8A0 7000CCA0 00801025 */   move  $v0, $a0
-.L7000CCA4:
-/* 00D8A4 7000CCA4 25080001 */  addiu $t0, $t0, 1
-/* 00D8A8 7000CCA8 A0600000 */  sb    $zero, ($v1)
-/* 00D8AC 7000CCAC 1448FFFD */  bne   $v0, $t0, .L7000CCA4
-/* 00D8B0 7000CCB0 24630001 */   addiu $v1, $v1, 1
-/* 00D8B4 7000CCB4 11100007 */  beq   $t0, $s0, .L7000CCD4
-.L7000CCB8:
-/* 00D8B8 7000CCB8 25080004 */   addiu $t0, $t0, 4
-/* 00D8BC 7000CCBC A0600001 */  sb    $zero, 1($v1)
-/* 00D8C0 7000CCC0 A0600002 */  sb    $zero, 2($v1)
-/* 00D8C4 7000CCC4 A0600003 */  sb    $zero, 3($v1)
-/* 00D8C8 7000CCC8 24630004 */  addiu $v1, $v1, 4
-/* 00D8CC 7000CCCC 1510FFFA */  bne   $t0, $s0, .L7000CCB8
-/* 00D8D0 7000CCD0 A060FFFC */   sb    $zero, -4($v1)
-.L7000CCD4:
-/* 00D8D4 7000CCD4 00604825 */  move  $t1, $v1
-/* 00D8D8 7000CCD8 25F90024 */  addiu $t9, $t7, 0x24
-.L7000CCDC:
-/* 00D8DC 7000CCDC 8DE10000 */  lw    $at, ($t7)
-/* 00D8E0 7000CCE0 25EF000C */  addiu $t7, $t7, 0xc
-/* 00D8E4 7000CCE4 2529000C */  addiu $t1, $t1, 0xc
-/* 00D8E8 7000CCE8 A921FFF4 */  swl   $at, -0xc($t1)
-/* 00D8EC 7000CCEC B921FFF7 */  swr   $at, -9($t1)
-/* 00D8F0 7000CCF0 8DE1FFF8 */  lw    $at, -8($t7)
-/* 00D8F4 7000CCF4 A921FFF8 */  swl   $at, -8($t1)
-/* 00D8F8 7000CCF8 B921FFFB */  swr   $at, -5($t1)
-/* 00D8FC 7000CCFC 8DE1FFFC */  lw    $at, -4($t7)
-/* 00D900 7000CD00 A921FFFC */  swl   $at, -4($t1)
-/* 00D904 7000CD04 15F9FFF5 */  bne   $t7, $t9, .L7000CCDC
-/* 00D908 7000CD08 B921FFFF */   swr   $at, -1($t1)
-/* 00D90C 7000CD0C 8DE10000 */  lw    $at, ($t7)
-/* 00D910 7000CD10 240A00FE */  li    $t2, 254
-/* 00D914 7000CD14 24630028 */  addiu $v1, $v1, 0x28
-/* 00D918 7000CD18 A9210000 */  swl   $at, ($t1)
-/* 00D91C 7000CD1C B9210003 */  swr   $at, 3($t1)
-/* 00D920 7000CD20 A06A0000 */  sb    $t2, ($v1)
-/* 00D924 7000CD24 8FBF001C */  lw    $ra, 0x1c($sp)
-/* 00D928 7000CD28 8FB00018 */  lw    $s0, 0x18($sp)
-/* 00D92C 7000CD2C 27BD0058 */  addiu $sp, $sp, 0x58
-/* 00D930 7000CD30 03E00008 */  jr    $ra
-/* 00D934 7000CD34 00000000 */   nop   
-)
-#endif
-
-
 
 #ifdef NONMATCHING
 s32 osMotorInit(OSMesgQueue *mq,OSPfs *pfs,int channel) {
