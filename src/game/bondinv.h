ebc6cc37dbaa7576ee1974ba99d69212f0cf7d71
thespider64 <spidersixtyfour@gmail.com>
2021-03-20 14:55:05 -0300

    Declaring functions in headers to avoid warnings

--- src/game/bondinv.h
+++ src/game/bondinv.h
@@ -6,4 +6,8 @@ void reinit_BONDdata_inventory(void);
 s32 bondinvIsAliveWithFlag(void);
 s32 count_total_items_in_inventory(void);
 
+InvItem *inv_get_item_by_index(s32 index);
+textoverride *get_textoverride_by_obj(ObjectRecord *obj);
+textoverride *get_textoverride_by_weaponum(ITEM_IDS weaponnum);
+
 #endif
