69ea6720f7dc31ceb558b0d8d35ada5504d999f5
Ben Burns <benjaminaburns@gmail.com>
2021-03-20 19:02:21 -0500

    nonmatching progress on video_related_7; osVi doc

--- include/PR/os_vi.h
+++ include/PR/os_vi.h
@@ -6,8 +6,16 @@
 
 /* Ultra64 Video Interface */
 
+/*
+ * Values for osTvType 
+ */
+#define    OS_TV_PAL        0
+#define    OS_TV_NTSC        1
+#define    OS_TV_MPAL        2
 
-/* Special Features */
+/*
+ * Video Interface (VI) special features
+ */
 #define OS_VI_GAMMA_ON 0x0001
 #define OS_VI_GAMMA_OFF 0x0002
 #define OS_VI_GAMMA_DITHER_ON 0x0004
@@ -17,12 +25,300 @@
 #define OS_VI_DITHER_FILTER_ON 0x0040
 #define OS_VI_DITHER_FILTER_OFF 0x0080
 
-#define OS_VI_GAMMA 0x08
-#define OS_VI_GAMMA_DITHER 0x04
-#define OS_VI_DIVOT 0x10
-#define OS_VI_DITHER_FILTER 0x10000
-#define OS_VI_UNK200 0x200
-#define OS_VI_UNK100 0x100
+/*
+ * Video Interface (VI) mode attribute bit
+ */
+#define OS_VI_BIT_NONINTERLACE        0x0001          /* lo-res */
+#define OS_VI_BIT_INTERLACE        0x0002          /* lo-res */
+#define OS_VI_BIT_NORMALINTERLACE    0x0004          /* hi-res */
+#define OS_VI_BIT_DEFLICKINTERLACE    0x0008          /* hi-res */
+#define OS_VI_BIT_ANTIALIAS        0x0010
+#define OS_VI_BIT_POINTSAMPLE        0x0020
+#define OS_VI_BIT_16PIXEL        0x0040
+#define OS_VI_BIT_32PIXEL        0x0080
+#define OS_VI_BIT_LORES            0x0100
+#define OS_VI_BIT_HIRES            0x0200
+#define OS_VI_BIT_NTSC            0x0400
+#define OS_VI_BIT_PAL            0x0800
+
+
+
+/**
+ * The VI is responsible for displaying the RDP-rendered images in different video modes. Currently,
+ * VI supports 42 video display modes -- 14 for NTSC, 14 for PAL, and 14 for MPAL. Each mode contains
+ * different settings to handle attributes such as interlaced and non- interlaced, 16-bit and 32-bit
+ * color pixel, and low and high resolution. Furthermore, a mode can be configured to rescale the
+ * image to sacrifice resolution for rendering speed.
+ * 
+ * These supported modes can be represented by 5 switches:
+ * 
+ *      high/low resolution
+ *      16-/32-bit color pixel
+ *      anti-aliased/point-sampled
+ *      filtered/not filtered
+ *      NTSC/PAL/MPAL format
+ * 
+ * There are symbolic names defined for these modes in <os.h>. For example, uhOpenGame represents
+ * an NTSC mode that supports low resolution, point sampled, non- interlaced, and 16-bit color
+ * pixel. Similarly, OS_VI_PAL_LPN1 refers to the same mode but in PAL format. The last
+ * four-character block (LPN1) in the symbol name is constructed by using the following codes for
+ * each character position:
+ * 
+ *      1st position is H for high resolution or L for low resolution
+ *      2nd position is A for anti-aliasing or P for point-sampled
+ *      3rd position for:
+ *          Low resolution is N for non-interlace or F for interlace
+ *          High resolution is N for normal-interlace F deflickered interlace
+ *      4th position is 1 for 16-bit pixel size or 2 for 32-bit pixel size
+ * 
+ * When you apply these codes, the following 14 modes are supported for NTSC as well as for
+ * PAL and MPAL for a total of 42 modes:
+ * 
+ *      LPN1   HPN1
+ *      LPF1   HPF1
+ *      LAN1   HAN1
+ *      LAF1   HAF1
+ *      LPN2   HPN2
+ *      LPF2   HPF2
+ *      LAN2   LAF2
+ * 
+ * In low resolution (320 pixels by 240 lines), you have a choice between non-interlaced and
+ * interlaced mode. Non-interlaced mode repeats the same frame each field. Interlaced mode
+ * interpolates between adjacent lines, weighting 75% of the line above plus 25% of the line
+ * below in the first field, and then weighting 25% of the line above plus 75% of the line
+ * below in the second field. Note that there is no flicker because there are no high spatial
+ * frequencies.
+ * 
+ * In high resolution (640 pixels by 480 lines), you have a choice between normal interlace
+ * and deflickered interlace. Normal interlace uses just the data rendered in one field to
+ * display that field. This mode can use one high resolution frame buffer without additional
+ * double buffering because one field can be displayed while the next is being rendered without
+ * stepping on each other. However, any single pixel with high detail will flicker because it
+ * is only displayed in one field. Deflickered interlace averages adjacent lines to filter out
+ * the high frequency vertical detail, but at the cost of requiring double buffering of the
+ * entire high resolution frame because both rendered fields are used to display each field. 
+ * 
+ * source: /Functions Reference Manual/os/osVi.html
+ */
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// NTSC
+
+/**
+ * NTSC, low resolution, point-sampled, not filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_LPN1		0	
+
+/**
+ * NTSC, low resolution, point-sampled, filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_LPF1		1
+
+/**
+ * NTSC, low resolution, anti-aliased, not filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_LAN1		2
+
+/**
+ * NTSC, low resolution, anti-aliased, filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_LAF1		3
+
+/**
+ * NTSC, low resolution, point-sampled, not filtered, 32 bit pixels
+ */
+#define OS_VI_NTSC_LPN2		4
+
+/**
+ * NTSC, low resolution, point-sampled, filtered, 32 bit pixels
+ */
+#define OS_VI_NTSC_LPF2		5
+
+/**
+ * NTSC, low resolution, anti-aliased, not filtered, 32 bit pixels
+ */
+#define OS_VI_NTSC_LAN2		6
+
+/**
+ * NTSC, low resolution, anti-aliased, filtered, 32 bit pixels
+ */
+#define OS_VI_NTSC_LAF2		7
+
+/**
+ * NTSC, high resolution, point-sampled, not filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_HPN1		8
+
+/**
+ * NTSC, high resolution, point-sampled, filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_HPF1		9
+
+/**
+ * NTSC, high resolution, anti-aliased, not filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_HAN1		10
+
+/**
+ * NTSC, high resolution, anti-aliased, filtered, 16 bit pixels
+ */
+#define OS_VI_NTSC_HAF1		11
+
+/**
+ * NTSC, high resolution, point-sampled, not filtered, 32 bit pixels
+ */
+#define OS_VI_NTSC_HPN2		12
+
+/**
+ * NTSC, high resolution, point-sampled, filtered, 32 bit pixels
+ */
+#define OS_VI_NTSC_HPF2		13
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// PAL
+
+/**
+ * PAL, low resolution, point-sampled, not filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_LPN1		14
+
+/**
+ * PAL, low resolution, point-sampled, filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_LPF1		15
+
+/**
+ * PAL, low resolution, anti-aliased, not filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_LAN1		16
+
+/**
+ * PAL, low resolution, anti-aliased, filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_LAF1		17
+
+/**
+ * PAL, low resolution, point-sampled, not filtered, 32 bit pixels
+ */
+#define OS_VI_PAL_LPN2		18
+
+/**
+ * PAL, low resolution, point-sampled, filtered, 32 bit pixels
+ */
+#define OS_VI_PAL_LPF2		19
+
+/**
+ * PAL, low resolution, anti-aliased, not filtered, 32 bit pixels
+ */
+#define OS_VI_PAL_LAN2		20
+
+/**
+ * PAL, low resolution, anti-aliased, filtered, 32 bit pixels
+ */
+#define OS_VI_PAL_LAF2		21
+
+/**
+ * PAL, high resolution, point-sampled, not filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_HPN1		22
+
+/**
+ * PAL, high resolution, point-sampled, filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_HPF1		23
+
+/**
+ * PAL, high resolution, anti-aliased, not filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_HAN1		24
+
+/**
+ * PAL, high resolution, anti-aliased, filtered, 16 bit pixels
+ */
+#define OS_VI_PAL_HAF1		25
+
+/**
+ * PAL, high resolution, point-sampled, not filtered, 32 bit pixels
+ */
+#define OS_VI_PAL_HPN2		26
+
+/**
+ * PAL, high resolution, point-sampled, filtered, 32 bit pixels
+ */
+#define OS_VI_PAL_HPF2		27
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// MPAL
+
+/**
+ * MPAL, low resolution, point-sampled, not filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_LPN1     28
+
+/**
+ * MPAL, low resolution, point-sampled, filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_LPF1     29
+
+/**
+ * MPAL, low resolution, anti-aliased, not filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_LAN1     30
+
+/**
+ * MPAL, low resolution, anti-aliased, filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_LAF1     31
+
+/**
+ * MPAL, low resolution, point-sampled, not filtered, 32 bit pixels
+ */
+#define OS_VI_MPAL_LPN2     32
+
+/**
+ * MPAL, low resolution, point-sampled, filtered, 32 bit pixels
+ */
+#define OS_VI_MPAL_LPF2     33
+
+/**
+ * MPAL, low resolution, anti-aliased, not filtered, 32 bit pixels
+ */
+#define OS_VI_MPAL_LAN2     34
+
+/**
+ * MPAL, low resolution, anti-aliased, filtered, 32 bit pixels
+ */
+#define OS_VI_MPAL_LAF2     35
+
+/**
+ * MPAL, high resolution, point-sampled, not filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_HPN1     36
+
+/**
+ * MPAL, high resolution, point-sampled, filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_HPF1     37
+
+/**
+ * MPAL, high resolution, anti-aliased, not filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_HAN1     38
+
+/**
+ * MPAL, high resolution, anti-aliased, filtered, 16 bit pixels
+ */
+#define OS_VI_MPAL_HAF1     39
+
+/**
+ * MPAL, high resolution, point-sampled, not filtered, 32 bit pixels
+ */
+#define OS_VI_MPAL_HPN2     40
+
+/**
+ * MPAL, high resolution, point-sampled, filtered, 32 bit pixels
+ */
+#define OS_VI_MPAL_HPF2     41
 
 
 /* Types */
@@ -73,60 +369,79 @@ typedef struct
     /* 0x2c */ u32 unk2c;
 } OSViContext;
 
-void osCreateViManager(OSPri pri);
+/**************************************************************************
+ *
+ * Extern variables
+ *
+ */
+extern OSViMode    osViModeTable[];    /* Global VI mode table */
+
+extern OSViMode    osViModeNtscLpn1;    /* Individual VI NTSC modes */
+extern OSViMode    osViModeNtscLpf1;
+extern OSViMode    osViModeNtscLan1;
+extern OSViMode    osViModeNtscLaf1;
+extern OSViMode    osViModeNtscLpn2;
+extern OSViMode    osViModeNtscLpf2;
+extern OSViMode    osViModeNtscLan2;
+extern OSViMode    osViModeNtscLaf2;
+extern OSViMode    osViModeNtscHpn1;
+extern OSViMode    osViModeNtscHpf1;
+extern OSViMode    osViModeNtscHan1;
+extern OSViMode    osViModeNtscHaf1;
+extern OSViMode    osViModeNtscHpn2;
+extern OSViMode    osViModeNtscHpf2;
+
+extern OSViMode    osViModePalLpn1;    /* Individual VI PAL modes */
+extern OSViMode    osViModePalLpf1;
+extern OSViMode    osViModePalLan1;
+extern OSViMode    osViModePalLaf1;
+extern OSViMode    osViModePalLpn2;
+extern OSViMode    osViModePalLpf2;
+extern OSViMode    osViModePalLan2;
+extern OSViMode    osViModePalLaf2;
+extern OSViMode    osViModePalHpn1;
+extern OSViMode    osViModePalHpf1;
+extern OSViMode    osViModePalHan1;
+extern OSViMode    osViModePalHaf1;
+extern OSViMode    osViModePalHpn2;
+extern OSViMode    osViModePalHpf2;
+
+extern OSViMode    osViModeMpalLpn1;    /* Individual VI MPAL modes */
+extern OSViMode    osViModeMpalLpf1;
+extern OSViMode    osViModeMpalLan1;
+extern OSViMode    osViModeMpalLaf1;
+extern OSViMode    osViModeMpalLpn2;
+extern OSViMode    osViModeMpalLpf2;
+extern OSViMode    osViModeMpalLan2;
+extern OSViMode    osViModeMpalLaf2;
+extern OSViMode    osViModeMpalHpn1;
+extern OSViMode    osViModeMpalHpf1;
+extern OSViMode    osViModeMpalHan1;
+extern OSViMode    osViModeMpalHaf1;
+extern OSViMode    osViModeMpalHpn2;
+extern OSViMode    osViModeMpalHpf2;
+
+/**************************************************************************
+ *
+ * Function prototypes
+ *
+ */
+
+/* Video interface (Vi) */
+u32 osViGetStatus(void);
+u32 osViGetCurrentMode(void);
+u32 osViGetCurrentLine(void);
+u32 osViGetCurrentField(void);
+void *osViGetCurrentFramebuffer(void);
+void *osViGetNextFramebuffer(void);
+void osViSetXScale(f32);
+void osViSetYScale(f32);
+void osViSetSpecialFeatures(u32 func);
 void osViSetMode(OSViMode *mode);
 void osViSetEvent(OSMesgQueue *mq, OSMesg msg, u32 retraceCount);
-void osViBlack(u8 active);
-void osViSetSpecialFeatures(u32 func);
 void osViSwapBuffer(void *vaddr);
-
-
-#define OS_VI_NTSC_LPN1		0	/* NTSC */
-#define OS_VI_NTSC_LPF1		1
-#define OS_VI_NTSC_LAN1		2
-#define OS_VI_NTSC_LAF1		3
-#define OS_VI_NTSC_LPN2		4
-#define OS_VI_NTSC_LPF2		5
-#define OS_VI_NTSC_LAN2		6
-#define OS_VI_NTSC_LAF2		7
-#define OS_VI_NTSC_HPN1		8
-#define OS_VI_NTSC_HPF1		9
-#define OS_VI_NTSC_HAN1		10
-#define OS_VI_NTSC_HAF1		11
-#define OS_VI_NTSC_HPN2		12
-#define OS_VI_NTSC_HPF2		13
-
-#define OS_VI_PAL_LPN1		14	/* PAL */
-#define OS_VI_PAL_LPF1		15
-#define OS_VI_PAL_LAN1		16
-#define OS_VI_PAL_LAF1		17
-#define OS_VI_PAL_LPN2		18
-#define OS_VI_PAL_LPF2		19
-#define OS_VI_PAL_LAN2		20
-#define OS_VI_PAL_LAF2		21
-#define OS_VI_PAL_HPN1		22
-#define OS_VI_PAL_HPF1		23
-#define OS_VI_PAL_HAN1		24
-#define OS_VI_PAL_HAF1		25
-#define OS_VI_PAL_HPN2		26
-#define OS_VI_PAL_HPF2		27
-
-// ? OS_VI_MPAL_LPN1 28	/* MPAL */
-// ? OS_VI_MPAL_LPF1 29
-#define OS_VI_MPAL_LAN1     30
-// ? OS_VI_MPAL_LAF1 31
-// ? OS_VI_MPAL_LPN2 32
-// ? OS_VI_MPAL_LPF2 33
-#define OS_VI_MPAL_LAN2     34
-// ? OS_VI_MPAL_LAF2 35
-// ? OS_VI_MPAL_HPN1 36
-// ? OS_VI_MPAL_HPF1 37
-// ? OS_VI_MPAL_HAN1 38
-#define OS_VI_MPAL_HAF1     39
-// ? OS_VI_MPAL_HPN2 40
-// ? OS_VI_MPAL_HPF2 41
-
-extern OSViMode	osViModeTable[];	/* Global VI mode table */
-
-
+void osViBlack(u8 active);
+void osViFade(u8, u16);
+void osViRepeatLine(u8);
+void osCreateViManager(OSPri);
 #endif
--- src/libultra/visetmode.c
+++ src/libultra/visetmode.c
@@ -2,6 +2,48 @@
 
 extern OSViContext *__osViNext;
 
+/**
+ * Currently, there is a global VI mode table (osViModeTable) that contains register settings for
+ * all 42 modes. You can use the osViSetMode function to register one of these modes with the VI
+ * to be used at the next vertical retrace. This function simply overwrites all the VI registers
+ * with data from the location pointed to by the mode argument, which is a pointer into
+ * osViModeTable. All previous settings (such as XY scaling and gamma correction) are reset. For
+ * example, to register the NTSC, low resolution, anti-aliased, non-interlaced, 16-bit mode with
+ * the VI, use this code:
+ * 
+ *      osViSetMode(osViModeTable[OS_VI_NTSC_LAN1]);
+ * 
+ * For storage efficiency, the global VI mode table is divided into 42 separate mode structures
+ * with each structure globally defined in a separate source file. These structures are
+ * externally declared in <os.h>. To gain access to the equivalent mode mentioned above, use
+ * this code:
+ * 
+ *      osViSetMode(osViModeNtscLan1);
+ * 
+ * When Nintendo 64 powers on, the boot code sets the timing to NTSC, PAL, or MPAL and blanks
+ * out the video. The call to osCreateViManager initializes the video to LAN1 (in either NTSC,
+ * PAL, or MPAL mode) and also blanks out the video. The first time video is enabled is when
+ * osViSetMode is called. To keep the screen blank until after the first use of osViSwapBuffer,
+ * call osViBlack(TRUE) immediately after the osViSetMode call. Then call osViBlack(FALSE) right
+ * before the first call to osViSwapBuffer.
+ * 
+ * Notes
+ * The VI manager must be started at the beginning of the program so that vertical retrace
+ * interrupts can be serviced as quickly as possible. Note that osViSetMode resets all current
+ * settings such as X-Y scaling and special features such as gamma to the default values
+ * defined in the global mode table.
+ * 
+ * The video back-end filter can only execute an anti-aliasing filter or a dither filter, but
+ * not both simultaneously. This is especially visible on "billboard" cutouts in texture edge
+ * mode during magnification in the region of the cutout where alpha goes from 1 to 0. In this
+ * region, the dither pattern is visible because the back-end filter executes an anti-alias
+ * filter (due to partial coverage) and turns off the dither filter.
+ * 
+ * This problem also occurs on anti-aliased silhouette edges of polygons, but it is not
+ * obvious due to the small width of the partially covered region. 
+ * 
+ * source: /Functions Reference Manual/os/osViSetMode.html
+ */
 void osViSetMode(OSViMode *mode) {
     register u32 int_disabled = __osDisableInt();
     __osViNext->modep = mode;
