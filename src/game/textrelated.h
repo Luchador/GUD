409e8ebfda57f3d6b1b199456a456001128bb52c
Ben Burns <benjaminaburns@gmail.com>
2022-02-03 14:04:32 -0600

    dirty match constructor_menu08_difficulty

--- src/game/textrelated.h
+++ src/game/textrelated.h
@@ -9,4 +9,7 @@ extern s32 ptrSecondFontTableLarge;
 
 void textrelatedInit_REMOVED(void);
 void load_font_tables(void);
+
+Gfx * microcode_constructor_related_to_menus(Gfx *, s32, s32, s32, s32, s32);
+void sub_GAME_7F0AE98C(s32 *y2, s32 *x2, u8 *text, s32 ptrSecondFontTableLarge, s32 ptrFirstFontTableLarge, s32 unk5);
 #endif
