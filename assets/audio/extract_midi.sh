41ef3b0a562fd906d9e34f1bd059d41582f3aa21
Ben Burns <benjaminaburns@gmail.com>
2022-02-28 22:07:49 -0600

    midi extract script

--- assets/audio/extract_midi.sh
+++ assets/audio/extract_midi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# set paths for commands. These are relative to the assets/audio folder.
 SBKSPLIT="../../tools/gaudio/bin/sbksplit"
 CSEQ2MIDI="../../tools/gaudio/bin/cseq2midi"
 INFLATE="../../tools/1172inflate.sh"
@@ -14,23 +15,43 @@ if [ ! -f "${CSEQ2MIDI}" ] ; then
     exit 1
 fi
 
+# create output directories.
+if [ ! -d "seqrz" ] ; then
+    mkdir "seqrz"
+fi
 if [ ! -d "seq" ] ; then
     mkdir "seq"
 fi
+if [ ! -d "midi" ] ; then
+    mkdir "midi"
+fi
 
-"${SBKSPLIT}" --in music.sbk -p seq/ --names music_names
+# Split soundbank into individual seq.rz files.
+# Output to assets/audio/seqrz
+# Use the `music_names` file in the assets/audio to name each music track.
+"${SBKSPLIT}" --in music.sbk -p seqrz/ --names music_names
 
-for file in seq/*.seq.rz
+# Foreach each .seq.rz file, 1172 inflate it to assets/audio/seq/
+# path is wonky, cd into folder then execute from there, adjusting paths, then cd out.
+cd seqrz
+for file in *.seq.rz
 do
     OUTPUT_FILENAME=$(echo "${file}" | sed -e 's/seq\.rz$/seq/')
     if [ "${file}" = "${OUTPUT_FILENAME}" ]; then
         echo "cannot determine what to rename file: ${file}"
         break
     fi
-    GZ=$(which gzip) "${INFLATE}" "${file}" "${OUTPUT_FILENAME}"
+    GZ=$(which gzip) "../${INFLATE}" "${file}" "../seq/${OUTPUT_FILENAME}"
 done
+cd ..
 
-for file in seq/*.seq
+# Foreach each .seq, convert to midi.
+# Place output in assets/audio/midi
+# Save pattern markers with same file name but .pattern extension in the same folder.
+# If conversion fails with fatal error, cd up, but print exit code.
+# path is wonky, cd into folder then execute from there, adjusting paths, then cd out.
+cd seq
+for file in *.seq
 do
     OUT_MIDI_FILENAME=$(echo "${file}" | sed -e 's/seq$/midi/')
     OUT_PATTERN_FILENAME=$(echo "${file}" | sed -e 's/seq$/pattern/')
@@ -43,10 +64,12 @@ do
         break
     fi
     echo "run cseq2midi on \"${file}\""
-    "${CSEQ2MIDI}" --in "${file}" --out "${OUT_MIDI_FILENAME}" --pattern-file "${OUT_PATTERN_FILENAME}"
+    "../${CSEQ2MIDI}" --in "${file}" --out "../midi/${OUT_MIDI_FILENAME}" --pattern-file "../midi/${OUT_PATTERN_FILENAME}"
     EXIT_CODE=$?
     if [ ${EXIT_CODE} -ne 0 ]; then
         echo "cseq2midi failed with exit code ${EXIT_CODE}"
+        cd ..
         exit ${EXIT_CODE}
     fi
 done
+cd ..
