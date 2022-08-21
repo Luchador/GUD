a1dedb2e503bbd54ec22032f5d7b95afc4c1e064
Trevor Leeming <trevor.leeming@live.co.uk>
2022-08-21 19:34:33 +0100

    testing GH Actions for datetime

--- tools/ge-stats.py
+++ tools/ge-stats.py
@@ -187,11 +187,13 @@ class StatResults:
 
 
 def mtime_os(file, now):
+    print('using os')
     return int(os.path.getmtime(file))
 
 
 
 def mtime_git(file, now):
+    print('using git')
     try:
         date_str = now.strftime('%Y-%m-%dT%H:%M:%S%z')
         result = subprocess.run(['git', 'log', '-1', '--format=\"%ct\"', '--date=local', '--before=\"' + date_str + '\"', '--', file], stdout=subprocess.PIPE, universal_newlines=True)
@@ -237,6 +239,10 @@ def process_source_files(search, stats: StatResults, mtime_resolver):
                 sfc.asm_functions = set()
                 sfc.parent = s
                 sfc.mtime = mtime_resolver(file, stats.now)
+                import datetime
+
+                x = datetime.datetime.fromtimestamp(sfc.mtime)
+                print(sfc.path, ' = ', x.strftime('%c'))
 
                 # The `completed` list is manually configured to specify which files should not be
                 # counted against the total. This is done by simply not adding asm function
