9c09594a549f639287613358b8285c9346acf79a
Ben Burns <benjaminaburns@gmail.com>
2023-01-22 17:05:53 -0600

    fix g_DamageTypes

--- src/game/bondview.h
+++ src/game/bondview.h
@@ -2784,7 +2784,7 @@ D:80036624                     .word 0xFFFFFF00, 0xFFFFFF00, 0x4FFFFFF
 //D:80036630
 extern u32 D_80036630;
 //D:80036634
-extern struct HealthDamageType g_HealthDamageTypes[];
+extern struct HealthDamageType g_DamageTypes[];
 /*
 D:80036638                     .byte 0
 D:80036639                     .byte 0, 0, 0xA
