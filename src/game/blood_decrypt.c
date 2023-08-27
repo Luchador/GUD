51e4426dbcd2e9a45ea2011f4ad3dc024bf979ce
Ben Burns <benjaminaburns@gmail.com>
2023-08-27 08:10:28 -0500

    match sub_GAME_7F01CEEC (thanks Ryan)

--- src/game/blood_decrypt.c
+++ src/game/blood_decrypt.c
@@ -125,158 +125,23 @@ void sub_GAME_7F01CCEC(u8 *arg0, u8 arg1, u8 arg2, u8 *arg3, u8 arg4)
 
 
 
-#ifdef NONMATCHING
 // Averages 4 pixel data, ending on "second" row.
+// Address 0x7F01CEEC NTSC
 // https://decomp.me/scratch/J5ivR 55% ... or 90%?
-void sub_GAME_7F01CEEC(u8*, s32, u8*) {
-#if 0
-
-    // logical implementation ~ 50% match
-
+void sub_GAME_7F01CEEC(u8 *arg0, s32 arg1, u8 *arg2)
+{
     s32 i;
     s32 j;
 
-    for (i = 1; i < arg1 - 1; i++)
-    {
-        for (j = 0; j < 0x5e; j++)
-        {
-            arg2[(i-1)*0x60 + j+0] = (arg0[(i-1)*0x60 + j+0] + arg0[(i-1)*0x60 + j+1] + arg0[(i+0)*0x60 + j+0] + arg0[(i+0)*0x60 + j+1] + 2) >> 2;
+    arg0 += 0x61;
+    arg2 += 0x61;
+
+    for (i = 1; i < arg1 - 1; i++, arg2 += 2, arg0 += 2) {
+        for (j = 1; j < 0x5f; j++, arg2++, arg0++) {
+            *arg2 = (arg0[-1] + arg0[0] + arg0[-0x61] + arg0[-0x60] + 2) >> 2;
         }
     }
-#endif
-
-#if 0
-
-    // best match implementation ~ 90% match
-
-    s32 i;
-    s32 j;
-
-    u8 *var_a0;
-    u8 *var_a2;
-
-    var_a0 = arg0 + 0x61;
-    var_a2 = arg2 + 0x61;
-
-    for (i = 1; i < arg1 - 1; i++)
-    {
-        if ((var_a0[-1] + var_a0[0] + var_a0[-0x61] + var_a0[-0x60] + 2));
-        
-        var_a2[0] = (var_a0[-1] + var_a0[0] + var_a0[-0x61] + var_a0[-0x60] + 2) >> 2;
-        var_a2[1] = (var_a0[0] + var_a0[1] + var_a0[-0x60] + var_a0[-0x5f] + 2) >> 2;
-
-        j = 3;
-        var_a2 += 2;
-        var_a0 += 2;
-    
-        while ( j != 0x5f )
-        {
-            if ((var_a0[-1] + var_a0[0] + var_a0[-0x61] + var_a0[-0x60] + 2));
-            
-            var_a2[0] = (var_a0[-1] + var_a0[0] + var_a0[-0x61] + var_a0[-0x60] + 2) >> 2;
-            var_a2[1] = (var_a0[0] + var_a0[1] + var_a0[-0x60] + var_a0[-0x5f] + 2) >> 2;
-            var_a2[2] = (var_a0[1] + var_a0[2] + var_a0[-0x5f] + var_a0[-0x5e] + 2) >> 2;
-            var_a2[3] = (var_a0[2] + var_a0[3] + var_a0[-0x5e] + var_a0[-0x5d] + 2) >> 2;
-
-            j+=4;
-            var_a2 += 4;
-            var_a0 += 4;
-        }
-
-        var_a2 += 2;
-        var_a0 += 2;
-    }
-#endif
 }
-#else
-GLOBAL_ASM(
-.text
-glabel sub_GAME_7F01CEEC
-/* 051A1C 7F01CEEC 24A3FFFF */  addiu $v1, $a1, -1
-/* 051A20 7F01CEF0 28610002 */  slti  $at, $v1, 2
-/* 051A24 7F01CEF4 24840061 */  addiu $a0, $a0, 0x61
-/* 051A28 7F01CEF8 24C60061 */  addiu $a2, $a2, 0x61
-/* 051A2C 7F01CEFC 14200049 */  bnez  $at, .L7F01D024
-/* 051A30 7F01CF00 24020001 */   li    $v0, 1
-/* 051A34 7F01CF04 2407005F */  li    $a3, 95
-.L7F01CF08:
-/* 051A38 7F01CF08 908EFFA0 */  lbu   $t6, -0x60($a0)
-/* 051A3C 7F01CF0C 908FFFFF */  lbu   $t7, -1($a0)
-/* 051A40 7F01CF10 90990000 */  lbu   $t9, ($a0)
-/* 051A44 7F01CF14 9089FF9F */  lbu   $t1, -0x61($a0)
-/* 051A48 7F01CF18 01CFC021 */  addu  $t8, $t6, $t7
-/* 051A4C 7F01CF1C 03194021 */  addu  $t0, $t8, $t9
-/* 051A50 7F01CF20 01095021 */  addu  $t2, $t0, $t1
-/* 051A54 7F01CF24 254B0002 */  addiu $t3, $t2, 2
-/* 051A58 7F01CF28 000B6083 */  sra   $t4, $t3, 2
-/* 051A5C 7F01CF2C A0CC0000 */  sb    $t4, ($a2)
-/* 051A60 7F01CF30 908E0000 */  lbu   $t6, ($a0)
-/* 051A64 7F01CF34 908DFFA1 */  lbu   $t5, -0x5f($a0)
-/* 051A68 7F01CF38 90980001 */  lbu   $t8, 1($a0)
-/* 051A6C 7F01CF3C 9088FFA0 */  lbu   $t0, -0x60($a0)
-/* 051A70 7F01CF40 01AE7821 */  addu  $t7, $t5, $t6
-/* 051A74 7F01CF44 01F8C821 */  addu  $t9, $t7, $t8
-/* 051A78 7F01CF48 03284821 */  addu  $t1, $t9, $t0
-/* 051A7C 7F01CF4C 252A0002 */  addiu $t2, $t1, 2
-/* 051A80 7F01CF50 000A5883 */  sra   $t3, $t2, 2
-/* 051A84 7F01CF54 A0CB0001 */  sb    $t3, 1($a2)
-/* 051A88 7F01CF58 24050003 */  li    $a1, 3
-/* 051A8C 7F01CF5C 24C60002 */  addiu $a2, $a2, 2
-/* 051A90 7F01CF60 24840002 */  addiu $a0, $a0, 2
-.L7F01CF64:
-/* 051A94 7F01CF64 908CFFA0 */  lbu   $t4, -0x60($a0)
-/* 051A98 7F01CF68 908DFFFF */  lbu   $t5, -1($a0)
-/* 051A9C 7F01CF6C 908F0000 */  lbu   $t7, ($a0)
-/* 051AA0 7F01CF70 9099FF9F */  lbu   $t9, -0x61($a0)
-/* 051AA4 7F01CF74 018D7021 */  addu  $t6, $t4, $t5
-/* 051AA8 7F01CF78 01CFC021 */  addu  $t8, $t6, $t7
-/* 051AAC 7F01CF7C 03194021 */  addu  $t0, $t8, $t9
-/* 051AB0 7F01CF80 25090002 */  addiu $t1, $t0, 2
-/* 051AB4 7F01CF84 00095083 */  sra   $t2, $t1, 2
-/* 051AB8 7F01CF88 A0CA0000 */  sb    $t2, ($a2)
-/* 051ABC 7F01CF8C 908BFFA1 */  lbu   $t3, -0x5f($a0)
-/* 051AC0 7F01CF90 908C0000 */  lbu   $t4, ($a0)
-/* 051AC4 7F01CF94 908E0001 */  lbu   $t6, 1($a0)
-/* 051AC8 7F01CF98 9098FFA0 */  lbu   $t8, -0x60($a0)
-/* 051ACC 7F01CF9C 016C6821 */  addu  $t5, $t3, $t4
-/* 051AD0 7F01CFA0 01AE7821 */  addu  $t7, $t5, $t6
-/* 051AD4 7F01CFA4 01F8C821 */  addu  $t9, $t7, $t8
-/* 051AD8 7F01CFA8 27280002 */  addiu $t0, $t9, 2
-/* 051ADC 7F01CFAC 00084883 */  sra   $t1, $t0, 2
-/* 051AE0 7F01CFB0 A0C90001 */  sb    $t1, 1($a2)
-/* 051AE4 7F01CFB4 908AFFA2 */  lbu   $t2, -0x5e($a0)
-/* 051AE8 7F01CFB8 908B0001 */  lbu   $t3, 1($a0)
-/* 051AEC 7F01CFBC 908D0002 */  lbu   $t5, 2($a0)
-/* 051AF0 7F01CFC0 908FFFA1 */  lbu   $t7, -0x5f($a0)
-/* 051AF4 7F01CFC4 014B6021 */  addu  $t4, $t2, $t3
-/* 051AF8 7F01CFC8 018D7021 */  addu  $t6, $t4, $t5
-/* 051AFC 7F01CFCC 01CFC021 */  addu  $t8, $t6, $t7
-/* 051B00 7F01CFD0 27190002 */  addiu $t9, $t8, 2
-/* 051B04 7F01CFD4 00194083 */  sra   $t0, $t9, 2
-/* 051B08 7F01CFD8 A0C80002 */  sb    $t0, 2($a2)
-/* 051B0C 7F01CFDC 908A0002 */  lbu   $t2, 2($a0)
-/* 051B10 7F01CFE0 9089FFA3 */  lbu   $t1, -0x5d($a0)
-/* 051B14 7F01CFE4 908C0003 */  lbu   $t4, 3($a0)
-/* 051B18 7F01CFE8 908EFFA2 */  lbu   $t6, -0x5e($a0)
-/* 051B1C 7F01CFEC 012A5821 */  addu  $t3, $t1, $t2
-/* 051B20 7F01CFF0 016C6821 */  addu  $t5, $t3, $t4
-/* 051B24 7F01CFF4 01AE7821 */  addu  $t7, $t5, $t6
-/* 051B28 7F01CFF8 25F80002 */  addiu $t8, $t7, 2
-/* 051B2C 7F01CFFC 0018C883 */  sra   $t9, $t8, 2
-/* 051B30 7F01D000 24A50004 */  addiu $a1, $a1, 4
-/* 051B34 7F01D004 A0D90003 */  sb    $t9, 3($a2)
-/* 051B38 7F01D008 24C60004 */  addiu $a2, $a2, 4
-/* 051B3C 7F01D00C 14A7FFD5 */  bne   $a1, $a3, .L7F01CF64
-/* 051B40 7F01D010 24840004 */   addiu $a0, $a0, 4
-/* 051B44 7F01D014 24420001 */  addiu $v0, $v0, 1
-/* 051B48 7F01D018 24C60002 */  addiu $a2, $a2, 2
-/* 051B4C 7F01D01C 1443FFBA */  bne   $v0, $v1, .L7F01CF08
-/* 051B50 7F01D020 24840002 */   addiu $a0, $a0, 2
-.L7F01D024:
-/* 051B54 7F01D024 03E00008 */  jr    $ra
-/* 051B58 7F01D028 00000000 */   nop   
-)
-#endif
 
 
 
