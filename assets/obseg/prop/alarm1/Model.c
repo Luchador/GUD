3583cc3cb08bb4aa07fc7b106b3dc2129987fd6d
KholdFuzion <kholdfuzion@gmail.com>
2024-02-15 12:26:51 -0500

    cleanup and comments for demo prop asset (alarm1)

--- assets/obseg/prop/alarm1/Model.c
+++ assets/obseg/prop/alarm1/Model.c
@@ -1,17 +1,29 @@
 #include <bondtypes.h>
+#include "assets/image_externs.h"
+
 #define TEXTURECOUNT 2
 #define VERTEXGROUPCOUNT0 12
 #define COLLISIONVERTEXCOUNT0 12
+
+extern ModelRoData_GroupRecord GroupRecord_0x060;
+extern ModelRoData_BoundingBoxRecord BoundingBoxRecord_0x07c;
+extern Vertex Vertex_ARRAY_0x098[VERTEXGROUPCOUNT0];
+extern Vertex Collision_Vertex_ARRAY_0x158[COLLISIONVERTEXCOUNT0];
+extern s16 POINT_USAGE_ARRAY_0x218[VERTEXGROUPCOUNT0];
+extern ModelRoData_DisplayList_CollisionRecord DLCollisionRecord_0x230[];
+extern Gfx GFX_ARRAY_PRIMARY_0x250[];
+extern Gfx GFX_ARRAY_SECONDARY_0x2B0[];
+
 //base address is 0x05000000
 ModelFileTextures proptextures[TEXTURECOUNT] = 
 {
-    {_image29_ID, 32, 32, 0x06, 0x0, 0x02, 0x0, 0x0},
-    {_image31_ID, 32, 32, 0x06, 0x0, 0x02, 0x0, 0x0}
+    {IMAGE_BUZZERSWITCH, 32, 32, 0x06, 0x0, 0x02, 0x0, 0x0},
+    {IMAGE_BELL, 32, 32, 0x06, 0x0, 0x02, 0x0, 0x0}
 };
-                        //  {opcode,                                            data,          parent, next, prev,           child}
-ModelNode ModelNode_0x018 = {        MODELNODE_OPCODE_GROUP,       GroupRecord_0x060,             0x0,  0x0,  0x0, ModelNode_0x030};
-ModelNode ModelNode_0x030 = {         MODELNODE_OPCODE_BBOX, BoundingBoxRecord_0x07c, ModelNode_0x018,  0x0,  0x0, ModelNode_0x048};
-ModelNode ModelNode_0x048 = {  MODELNODE_OPCODE_DLCOLLISION, DLCollisionRecord_0x230, ModelNode_0x030,  0x0,  0x0,             0x0};
+                        //  {                        opcode,                     data,           parent, next, prev,            child}
+ModelNode ModelNode_0x018 = {        MODELNODE_OPCODE_GROUP,       &GroupRecord_0x060,              0x0,  0x0,  0x0, &ModelNode_0x030};
+ModelNode ModelNode_0x030 = {         MODELNODE_OPCODE_BBOX, &BoundingBoxRecord_0x07c, &ModelNode_0x018,  0x0,  0x0, &ModelNode_0x048};
+ModelNode ModelNode_0x048 = {  MODELNODE_OPCODE_DLCOLLISION, &DLCollisionRecord_0x230, &ModelNode_0x030,  0x0,  0x0,              0x0};
 
 ModelRoData_GroupRecord GroupRecord_0x060 = 
 {
@@ -71,31 +83,32 @@ s16 POINT_USAGE_ARRAY_0x218[VERTEXGROUPCOUNT0] =
 
 ModelRoData_DisplayList_CollisionRecord DLCollisionRecord_0x230 = 
 {
-    GFX_ARRAY_PRIMARY_0x250, 
-    GFX_ARRAY_SECONDARY_0x2B0, 
-    Vertex_ARRAY_0x098, VERTEXGROUPCOUNT0, 
-    COLLISIONVERTEXCOUNT0, Collision_Vertex_ARRAY_0x158, 
-    POINT_USAGE_ARRAY_0x218, 
-    0x4, 0x0, 0x0, 0x0
+    GFX_ARRAY_PRIMARY_0x250, //primary
+    GFX_ARRAY_SECONDARY_0x2B0, //secondary
+    Vertex_ARRAY_0x098, VERTEXGROUPCOUNT0, //vertices,vcount 
+    COLLISIONVERTEXCOUNT0, Collision_Vertex_ARRAY_0x158,  //ncolvtx,collision vertices
+    POINT_USAGE_ARRAY_0x218, //point usage
+    0x4, 0x0, //type, index
+    0x0 //baseaddr
 };
 
-Gfx GFX_ARRAY_PRIMARY_0x250 = 
+Gfx GFX_ARRAY_PRIMARY_0x250[] = 
 {
     gsDPSetTextureLOD(G_TL_LOD),
-    gsSPTexture(0xFFFF, 0xFFFF, 6, 0, true),
+    gsSPTexture(0xFFFF, 0xFFFF, 6, 0, TRUE),
     gsDPLoadSync(),
     gsDPSetTextureLOD(G_TL_LOD),
-    gsSPTexture(0xFFFF, 0xFFFF, 6, 0, true),
+    gsSPTexture(0xFFFF, 0xFFFF, 6, 0, TRUE),
     gsDPSetTextureDetail(G_TD_CLAMP),
     gsDPSetTextureFilter(G_TF_BILERP),
     gsSPEndDisplayList(),
 };
 
-Gfx GFX_ARRAY_SECONDARY_0x2B0 =
+Gfx GFX_ARRAY_SECONDARY_0x2B0[] =
 {
     gsDPLoadSync(),
     gsDPSetTextureLOD(G_TL_LOD),
-    gsSPTexture(0xFFFF, 0xFFFF, 6, 0, true),
+    gsSPTexture(0xFFFF, 0xFFFF, 6, 0, TRUE),
     gsDPSetTextureDetail(G_TD_CLAMP),
     gsDPSetTextureFilter(G_TF_BILERP),
     gsSPEndDisplayList(),
