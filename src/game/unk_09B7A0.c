86c818ead5c66ec2811709703a7b50a67cbefda8
Ben Burns <benjaminaburns@gmail.com>
2024-02-26 21:38:36 -0600

    improve sub_GAME_7F056690

--- src/game/unk_09B7A0.c
+++ src/game/unk_09B7A0.c
@@ -2,6 +2,7 @@
 #include <memp.h>
 #include "bondtypes.h"
 #include "unk_09B7A0.h"
+#include "chrobjhandler.h"
 
 // unsure if these structs are defined as something else, elsewhere
 struct unk_09B7A0_struct_child {
