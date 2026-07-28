b64899585ec69bbb72133037d69978362b0bb555
jakzo <jack@jf.id.au>
2026-07-29 09:33:57 +1000

    Match mempAddEntryOfSizeToBank (#16)

--- src/memp.c
+++ src/memp.c
@@ -269,123 +269,57 @@ glabel mempAllocBytesInBank
 )
 #endif
 
-#ifdef NONMATCHING
-// Mostly regalloc
-// https://decomp.me/scratch/IkY1t 98.98%
-s32 mempAddEntryOfSizeToBank(u8* ptrdata, u32 newsize, u8 bank)
+/**
+ * Resize the most recent allocation in a pool without moving it.
+ */
+MEMP_ADD_ENTRY_RESULT mempAddEntryOfSizeToBank(void *allocation, s32 newsize, u8 poolnum)
 {
-    s32 oldsize;
-    s32 diffsize;
+    MemoryPool *pool;
+    s32 origsize;
+    s32 growsize;
 
-    if (needmemallocation && (ptrdata == (u8*)g_mempPools[6].prevpos)) {
-        bank = 6;
+    if (needmemallocation && allocation == g_mempPools[MEMPOOL_PERMANENT].prevpos)
+    {
+        poolnum = MEMPOOL_PERMANENT;
     }
 
-    if (g_mempPools[bank].pos == 0) {
-        while(TRUE) {}
+    allocation = (void *)(u64)allocation;
+    pool = &g_mempPools[poolnum];
+
+    if (pool->pos == 0)
+    {
+        while (TRUE);
     }
 
-    if (ptrdata != (u8*)g_mempPools[bank].prevpos) {
-        return 2;
+    if (allocation != pool->prevpos)
+    {
+        return MEMP_ADD_ENTRY_NOT_LAST_ALLOCATION;
     }
 
-    oldsize = (g_mempPools[bank].pos - g_mempPools[bank].prevpos);
-    diffsize = (newsize - oldsize);
+    origsize = pool->pos - pool->prevpos;
+    growsize = newsize - origsize;
 
-    if (diffsize <= 0) {
-        g_mempPools[bank].pos += diffsize;
-        return 1;
+    if (growsize <= 0)
+    {
+        pool->pos += growsize;
+        return MEMP_ADD_ENTRY_SUCCESS;
     }
 
-    if (g_mempPools[bank].pos > g_mempPools[bank].end) {
+    if (pool->pos > pool->end)
+    {
         nulled_mempLoopAllMemBanks();
-        while(TRUE) {}
+        while (TRUE);
     }
 
-    if (g_mempPools[bank].pos + diffsize > g_mempPools[bank].end) {
+    if (pool->pos + growsize > pool->end)
+    {
         nulled_mempLoopAllMemBanks();
-        while(TRUE) {}
+        while (TRUE);
     }
 
-    g_mempPools[bank].pos += diffsize;
-
-    return 1;
+    pool->pos += growsize;
+    return MEMP_ADD_ENTRY_SUCCESS;
 }
-#else
-GLOBAL_ASM(
-.text
-glabel mempAddEntryOfSizeToBank
-/* 00A404 70009804 3C0F8002 */  lui   $t7, %hi(needmemallocation)
-/* 00A408 70009808 8DEF4404 */  lw    $t7, %lo(needmemallocation)($t7)
-/* 00A40C 7000980C 27BDFFE8 */  addiu $sp, $sp, -0x18
-/* 00A410 70009810 AFA60020 */  sw    $a2, 0x20($sp)
-/* 00A414 70009814 30CE00FF */  andi  $t6, $a2, 0xff
-/* 00A418 70009818 01C03025 */  move  $a2, $t6
-/* 00A41C 7000981C AFBF0014 */  sw    $ra, 0x14($sp)
-/* 00A420 70009820 AFA5001C */  sw    $a1, 0x1c($sp)
-/* 00A424 70009824 11E00006 */  beqz  $t7, .L70009840
-/* 00A428 70009828 00803825 */   move  $a3, $a0
-/* 00A42C 7000982C 3C188006 */  lui   $t8, %hi(g_mempPools+0x6C)
-/* 00A430 70009830 8F183C1C */  lw    $t8, %lo(g_mempPools+0x6C)($t8)
-/* 00A434 70009834 14980002 */  bne   $a0, $t8, .L70009840
-/* 00A438 70009838 00000000 */   nop
-/* 00A43C 7000983C 24060006 */  li    $a2, 6
-.L70009840:
-/* 00A440 70009840 3C0A8006 */  lui   $t2, %hi(g_mempPools)
-/* 00A444 70009844 254A3BB0 */  addiu $t2, %lo(g_mempPools) # addiu $t2, $t2, 0x3bb0
-/* 00A448 70009848 0006C900 */  sll   $t9, $a2, 4
-/* 00A44C 7000984C 032A2821 */  addu  $a1, $t9, $t2
-/* 00A450 70009850 8CA40004 */  lw    $a0, 4($a1)
-/* 00A454 70009854 54800004 */  bnezl $a0, .L70009868
-/* 00A458 70009858 8CA6000C */   lw    $a2, 0xc($a1)
-.L7000985C:
-/* 00A45C 7000985C 1000FFFF */  b     .L7000985C
-/* 00A460 70009860 00000000 */   nop
-/* 00A464 70009864 8CA6000C */  lw    $a2, 0xc($a1)
-.L70009868:
-/* 00A468 70009868 8FAB001C */  lw    $t3, 0x1c($sp)
-/* 00A46C 7000986C 50E60004 */  beql  $a3, $a2, .L70009880
-/* 00A470 70009870 00861023 */   subu  $v0, $a0, $a2
-/* 00A474 70009874 1000001A */  b     .L700098E0
-/* 00A478 70009878 24020002 */   li    $v0, 2
-/* 00A47C 7000987C 00861023 */  subu  $v0, $a0, $a2
-.L70009880:
-/* 00A480 70009880 01621823 */  subu  $v1, $t3, $v0
-/* 00A484 70009884 1C600004 */  bgtz  $v1, .L70009898
-/* 00A488 70009888 00836021 */   addu  $t4, $a0, $v1
-/* 00A48C 7000988C ACAC0004 */  sw    $t4, 4($a1)
-/* 00A490 70009890 10000013 */  b     .L700098E0
-/* 00A494 70009894 24020001 */   li    $v0, 1
-.L70009898:
-/* 00A498 70009898 8CA20008 */  lw    $v0, 8($a1)
-/* 00A49C 7000989C 00836821 */  addu  $t5, $a0, $v1
-/* 00A4A0 700098A0 0044082B */  sltu  $at, $v0, $a0
-/* 00A4A4 700098A4 50200006 */  beql  $at, $zero, .L700098C0
-/* 00A4A8 700098A8 004D082B */   sltu  $at, $v0, $t5
-/* 00A4AC 700098AC 0C00263C */  jal   nulled_mempLoopAllMemBanks
-/* 00A4B0 700098B0 24060006 */   li    $a2, 6
-.L700098B4:
-/* 00A4B4 700098B4 1000FFFF */  b     .L700098B4
-/* 00A4B8 700098B8 00000000 */   nop
-/* 00A4BC 700098BC 004D082B */  sltu  $at, $v0, $t5
-.L700098C0:
-/* 00A4C0 700098C0 10200005 */  beqz  $at, .L700098D8
-/* 00A4C4 700098C4 00837021 */   addu  $t6, $a0, $v1
-/* 00A4C8 700098C8 0C00263C */  jal   nulled_mempLoopAllMemBanks
-/* 00A4CC 700098CC 24060006 */   li    $a2, 6
-.L700098D0:
-/* 00A4D0 700098D0 1000FFFF */  b     .L700098D0
-/* 00A4D4 700098D4 00000000 */   nop
-.L700098D8:
-/* 00A4D8 700098D8 ACAE0004 */  sw    $t6, 4($a1)
-/* 00A4DC 700098DC 24020001 */  li    $v0, 1
-.L700098E0:
-/* 00A4E0 700098E0 8FBF0014 */  lw    $ra, 0x14($sp)
-/* 00A4E4 700098E4 27BD0018 */  addiu $sp, $sp, 0x18
-/* 00A4E8 700098E8 03E00008 */  jr    $ra
-/* 00A4EC 700098EC 00000000 */   nop
-)
-#endif
 
 void nulled_mempLoopAllMemBanks(void) {
     u8 bank;
