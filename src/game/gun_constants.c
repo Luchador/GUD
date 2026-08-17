eb3d8cb1ddf3125b258fc987dfeffc261b23d05f
Luchador <luchadordev@gmail.com>
2026-08-16 20:20:39 -0400

    Split gun.c to eliminate asm blocks

---
+++ src/game/gun_constants.c
@@ -0,0 +1,44 @@
+#include <ultra64.h>
+
+#if VERSION_EU
+const u32 g_GunScreenAspectRatio[1] = { 0x3f969697 };
+#else
+const u32 g_GunScreenAspectRatio[1] = { 0x3faaaaab };
+#endif
+
+const u32 D_800543B4[1] = { 0x3dccccce };
+const u32 D_800543B8[1] = { 0x3f088888 };
+const u32 D_800543BC[1] = { 0x40c90fdb };
+const u32 D_800543C0[1] = { 0x3ec90fdb };
+const u32 D_800543C4[1] = { 0x40c90fdb };
+const u32 D_800543C8[1] = { 0x3ec90fdb };
+const u32 D_800543CC[1] = { 0x40c90fdb };
+const u32 D_800543D0[1] = { 0x3ec90fdb };
+#if VERSION_EU
+const u32 D_800543D4[1] = { 0x49634ea0 };
+#else
+const u32 D_800543D4[1] = { 0x493d6c30 };
+#endif
+const u32 expended_shell_initial_gravity_modifier_pistol[1] = { 0x3e8e38e4 };
+const u32 D_800543DC[1] = { 0x3fb55555 };
+const u32 D_800543E0[1] = { 0x3fd55555 };
+const u32 D_800543E4[1] = { 0x40c90fdb };
+const u32 D_800543E8[1] = { 0x3ec90fdb };
+const u32 D_800543EC[1] = { 0x40c90fdb };
+const u32 D_800543F0[1] = { 0x3ec90fdb };
+const u32 D_800543F4[1] = { 0x40c90fdb };
+const u32 D_800543F8[1] = { 0x3ec90fdb };
+#if VERSION_EU
+const u32 D_800543FC[1] = { 0x49634ea0 };
+#else
+const u32 D_800543FC[1] = { 0x493d6c30 };
+#endif
+const u32 expended_shell_initial_gravity_modifier_non_pistol[1] = { 0x3e8e38e4 };
+const u32 g_CasingGravity[1] = { 0x3e8e38e4 };
+const u32 g_CasingModelScale[1] = { 0x3dccccce };
+const u32 g_CasingMinMatrixTranslation[1] = { 0xc6ea6000 };
+const u32 g_CasingMaxMatrixTranslation[1] = { 0x46ea6000 };
+
+#if VERSION_EU
+const u32 g_GunSightAspectRatio[1] = { 0x3f986186 };
+#endif
