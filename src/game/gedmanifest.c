8ecb7cd328b41997c36170aae1ba20c0e3334e2a
Luchador <luchadordev@gmail.com>
2026-09-04 19:07:37 -0400

    Finish updating manifest

--- src/game/gedmanifest.c
+++ src/game/gedmanifest.c
@@ -5,7 +5,6 @@ extern u8 _imagesSegmentRomStart[], _imagesSegmentRomEnd[];
 extern u8 _obsegSegmentRomStart[], _obsegSegmentRomEnd[];
 extern u8 _musicfilesSegmentRomStart[], _musicfilesSegmentRomEnd[];
 extern u8 _gedLevelTableRom[];
-extern u8 _csegmentSegmentRomStart[], _csegmentSegmentRomEnd[], _csegmentSegmentStart[];
 
 #define GEDM_KIND(a, b, c, d) \
     (((u32)(a) << 24) | ((u32)(b) << 16) | ((u32)(c) << 8) | (u32)(d))
@@ -35,6 +34,6 @@ const GedManifest g_GedManifest = {
         { GEDM_KIND('O','B','S','G'), (u32)_obsegSegmentRomStart,      (u32)_obsegSegmentRomEnd,                       0                          },
         { GEDM_KIND('M','U','S','F'), (u32)_musicfilesSegmentRomStart, (u32)_musicfilesSegmentRomEnd,                  GEDM_F_1172                },
         { GEDM_KIND('S','T','G','T'), (u32)_gedLevelTableRom,          (u32)_gedLevelTableRom + STAGES_MAX * 32,       STAGES_MAX                 },
-        { GEDM_KIND('C','M','A','P'), (u32)_csegmentSegmentRomStart,   (u32)_csegmentSegmentRomEnd,                    (u32)_csegmentSegmentStart },
+        { GEDM_KIND('C','M','A','P'), (u32)&_csegmentSegmentRomStart,  (u32)&_csegmentSegmentRomEnd,                   (u32)&_csegmentSegmentStart},
     },
 };
